586cc32b35cca958e34970a23ba8d2a460be9bbf
vitaliyboyko <vitaliyboyko@i.ua>
2017-12-02 16:23:58 +0200

    Fixed fixture source items rollback

--- app/code/Magento/InventoryApi/Test/_files/source_items_rollback.php
+++ app/code/Magento/InventoryApi/Test/_files/source_items_rollback.php
@@ -6,18 +6,19 @@
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\InventoryApi\Api\Data\SourceItemInterface;
 use Magento\InventoryApi\Api\SourceItemRepositoryInterface;
+use Magento\InventoryApi\Api\SourceItemsDeleteInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 
 /** @var SourceItemRepositoryInterface $sourceItemRepository */
 $sourceItemRepository = Bootstrap::getObjectManager()->get(SourceItemRepositoryInterface::class);
 /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
 $searchCriteriaBuilder = Bootstrap::getObjectManager()->get(SearchCriteriaBuilder::class);
+/** @var SourceItemsDeleteInterface $sourceItemDelete */
+$sourceItemDelete = Bootstrap::getObjectManager()->get(SourceItemsDeleteInterface::class);
 
 $searchCriteria = $searchCriteriaBuilder
     ->addFilter(SourceItemInterface::SKU, ['SKU-1', 'SKU-2', 'SKU-3'], 'in')
     ->create();
 $sourceItems = $sourceItemRepository->getList($searchCriteria)->getItems();
 
-foreach ($sourceItems as $sourceItem) {
-    $sourceItemRepository->delete($sourceItem);
-}
+$sourceItemDelete->execute($sourceItems);
