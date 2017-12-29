d55e388f480565cc37b63012c81a3ddfd42e7d05
larsroettig <l.roettig@techdivision.com>
2017-12-29 13:50:26 +0100

    FIX phpunit tests after refactoring

--- app/code/Magento/CatalogSearch/Model/Search/FilterMapper/TermDropdownStrategy.php
+++ app/code/Magento/CatalogSearch/Model/Search/FilterMapper/TermDropdownStrategy.php
@@ -97,7 +97,7 @@ class TermDropdownStrategy implements FilterStrategyInterface
             []
         );
         if ($this->isAddStockFilter()) {
-          $this->joinProvider->get($select, $alias);
+          $this->joinProvider->add($select, $alias);
         }
 
         return true;
