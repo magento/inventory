2e93a3bfd7a2381e214b13908e6281466fbdb92e
Stepa Furman <devops.magento@gmail.com>
2018-02-08 23:38:05 +0200

    added StockConfigurationInterface

--- app/code/Magento/Inventory/Model/IsProductSalable.php
+++ app/code/Magento/Inventory/Model/IsProductSalable.php
@@ -14,6 +14,7 @@ use Magento\CatalogInventory\Model\Configuration;
 use Magento\CatalogInventory\Model\Stock\Item as LegacyStockItem;
 use Magento\CatalogInventory\Model\Stock\StockItemRepository as LegacyStockItemRepository;
 use Magento\InventoryApi\Api\IsProductSalableInterface;
+use Magento\InventoryConfiguration\Model\StockConfigurationInterface;
 
 /**
  * @inheritdoc
@@ -51,12 +52,19 @@ class IsProductSalable implements IsProductSalableInterface
     private $stockItemCriteriaFactory;
 
     /**
+     * @var StockConfigurationInterface
+     */
+    private $stockConfiguration;
+
+    /**
+     * IsProductSalable constructor.
      * @param GetStockItemDataInterface $getStockItemData
      * @param GetReservationsQuantityInterface $getReservationsQuantity
      * @param Configuration $configuration
      * @param LegacyStockItemRepository $legacyStockItemRepository
      * @param ProductResourceModel $productResource
      * @param StockItemCriteriaInterfaceFactory $stockItemCriteriaFactory
+     * @param StockConfigurationInterface $stockConfiguration
      */
     public function __construct(
         GetStockItemDataInterface $getStockItemData,
@@ -64,7 +72,8 @@ class IsProductSalable implements IsProductSalableInterface
         Configuration $configuration,
         LegacyStockItemRepository $legacyStockItemRepository,
         ProductResourceModel $productResource,
-        StockItemCriteriaInterfaceFactory $stockItemCriteriaFactory
+        StockItemCriteriaInterfaceFactory $stockItemCriteriaFactory,
+        StockConfigurationInterface $stockConfiguration
     ) {
         $this->getStockItemData = $getStockItemData;
         $this->getReservationsQuantity = $getReservationsQuantity;
@@ -72,6 +81,7 @@ class IsProductSalable implements IsProductSalableInterface
         $this->legacyStockItemRepository = $legacyStockItemRepository;
         $this->productResource = $productResource;
         $this->stockItemCriteriaFactory = $stockItemCriteriaFactory;
+        $this->stockConfiguration = $stockConfiguration;
     }
 
     /**
@@ -92,49 +102,6 @@ class IsProductSalable implements IsProductSalableInterface
             return false;
         }
 
-        if ($this->isManageStock($legacyStockItem)) {
-            if (($legacyStockItem->getUseConfigMinQty() == 1 && $qtyWithReservation <= $globalMinQty)
-                || ($legacyStockItem->getUseConfigMinQty() == 0 && $qtyWithReservation <= $legacyStockItem->getMinQty())
-            ) {
-                $isSalable = false;
-            }
-        }
-
-        return $isSalable;
-    }
-
-    /**
-     * @param LegacyStockItem $legacyStockItem
-     *
-     * @return bool
-     */
-    private function isManageStock(LegacyStockItem $legacyStockItem): bool
-    {
-        $globalManageStock = $this->configuration->getManageStock();
-        $manageStock = false;
-        if (($legacyStockItem->getUseConfigManageStock() == 1 && $globalManageStock == 1)
-            || ($legacyStockItem->getUseConfigManageStock() == 0 && $legacyStockItem->getManageStock() == 1)
-        ) {
-            $manageStock = true;
-        }
-
-        return $manageStock;
-    }
-
-    /**
-     * @param string $sku
-     *
-     * @return LegacyStockItem|null
-     */
-    private function getLegacyStockItem(string $sku)
-    {
-        $productIds = $this->productResource->getProductsIdsBySkus([$sku]);
-        $searchCriteria = $this->stockItemCriteriaFactory->create();
-        $searchCriteria->addFilter(StockItemInterface::PRODUCT_ID, StockItemInterface::PRODUCT_ID, $productIds[$sku]);
-
-        $legacyStockItem = $this->legacyStockItemRepository->getList($searchCriteria);
-        $items = $legacyStockItem->getItems();
-
-        return count($items) ? reset($items) : null;
+        return $this->stockConfiguration->validate($sku, $stockId, $qtyWithReservation, $isSalable, $globalMinQty);
     }
 }
