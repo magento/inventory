2f1dced565c583833005dcf81386d01a9faebb36
Bartłomiej Szymański <bartlomiej.szymanski@bold.net.pl>
2018-03-24 12:26:09 +0100

    MSI-693 changed to itemQty

--- app/code/Magento/InventorySales/Plugin/StockState/CheckQuoteItemQtyPlugin.php
+++ app/code/Magento/InventorySales/Plugin/StockState/CheckQuoteItemQtyPlugin.php
@@ -102,7 +102,7 @@ class CheckQuoteItemQtyPlugin
         $result = $this->objectFactory->create();
         $result->setHasError(false);
 
-        $qty = $this->getNumber($origQty);
+        $qty = $this->getNumber($itemQty);
 
         $skus = $this->getSkusByProductIds->execute([$productId]);
         $productSku = $skus[$productId];
