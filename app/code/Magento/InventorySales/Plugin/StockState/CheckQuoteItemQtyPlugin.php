9f6f4d30b3ba05f36f12f4e5e8e14b067f4bf2df
Bartłomiej Szymański <bartlomiej.szymanski@bold.net.pl>
2018-03-24 11:51:07 +0100

    MSI-693 I can add to the cart product as many times as i want

--- app/code/Magento/InventorySales/Plugin/StockState/CheckQuoteItemQtyPlugin.php
+++ app/code/Magento/InventorySales/Plugin/StockState/CheckQuoteItemQtyPlugin.php
@@ -102,7 +102,7 @@ class CheckQuoteItemQtyPlugin
         $result = $this->objectFactory->create();
         $result->setHasError(false);
 
-        $qty = $this->getNumber($qtyToCheck);
+        $qty = $this->getNumber($origQty);
 
         $skus = $this->getSkusByProductIds->execute([$productId]);
         $productSku = $skus[$productId];
