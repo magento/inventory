6181519cc07d27624e8ba6f6c7bff125de628063
RomanKis <romaikiss@gmail.com>
2018-04-23 14:26:26 +0300

    MSI-878: Notify Quantity applied to Product in Advanced Inventory on Product page in Admin doesn't work

--- app/code/Magento/InventoryLowQuantityNotification/Observer/ProcessSourceItemConfigurationsObserver.php
+++ app/code/Magento/InventoryLowQuantityNotification/Observer/ProcessSourceItemConfigurationsObserver.php
@@ -9,8 +9,11 @@ namespace Magento\InventoryLowQuantityNotification\Observer;
 
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Controller\Adminhtml\Product\Save;
+use Magento\CatalogInventory\Ui\DataProvider\Product\Form\Modifier\AdvancedInventory;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer as EventObserver;
+use Magento\InventoryCatalog\Model\DefaultSourceProvider;
+use Magento\InventoryCatalog\Model\IsSingleSourceModeInterface;
 use Magento\InventoryConfiguration\Model\IsSourceItemsAllowedForProductTypeInterface;
 
 /**
@@ -32,15 +35,31 @@ class ProcessSourceItemConfigurationsObserver implements ObserverInterface
     private $sourceItemsConfigurationProcessor;
 
     /**
+     * @var IsSingleSourceModeInterface
+     */
+    private $isSingleSourceMode;
+
+    /**
+     * @var DefaultSourceProvider
+     */
+    private $defaultSourceProvider;
+
+    /**
      * @param IsSourceItemsAllowedForProductTypeInterface $isSourceItemsAllowedForProductType
      * @param SourceItemsConfigurationProcessor $sourceItemsConfigurationProcessor
+     * @param IsSingleSourceModeInterface $isSingleSourceMode
+     * @param DefaultSourceProvider $defaultSourceProvider
      */
     public function __construct(
         IsSourceItemsAllowedForProductTypeInterface $isSourceItemsAllowedForProductType,
-        SourceItemsConfigurationProcessor $sourceItemsConfigurationProcessor
+        SourceItemsConfigurationProcessor $sourceItemsConfigurationProcessor,
+        IsSingleSourceModeInterface $isSingleSourceMode,
+        DefaultSourceProvider $defaultSourceProvider
     ) {
         $this->isSourceItemsAllowedForProductType = $isSourceItemsAllowedForProductType;
         $this->sourceItemsConfigurationProcessor = $sourceItemsConfigurationProcessor;
+        $this->isSingleSourceMode = $isSingleSourceMode;
+        $this->defaultSourceProvider = $defaultSourceProvider;
     }
 
     /**
@@ -58,11 +77,50 @@ class ProcessSourceItemConfigurationsObserver implements ObserverInterface
         /** @var Save $controller */
         $controller = $observer->getEvent()->getController();
 
-        $sources = $controller->getRequest()->getParam('sources', []);
+        if ($this->isSingleSourceMode->execute() === true) {
+            $productData = $controller->getRequest()->getParam('product', []);
+
+            $this->processSourceItemConfigurationInSingleSourceMode($productData, $product->getSku());
+        } else {
+            $sources = $controller->getRequest()->getParam('sources', []);
+
+            $this->processSourceItemsConfigurationInMultiSourceMode($sources, $product->getSku());
+        }
+    }
+
+    /**
+     * @param array $productData
+     * @param string $sku
+     *
+     * @return void
+     */
+    private function processSourceItemConfigurationInSingleSourceMode(array $productData, string $sku): void
+    {
+        if (isset($productData[AdvancedInventory::STOCK_DATA_FIELDS])) {
+            $stockData = $productData[AdvancedInventory::STOCK_DATA_FIELDS];
+
+            $sourceItemData = [
+                'source_code' => $this->defaultSourceProvider->getCode(),
+                'notify_stock_qty_use_default' => $stockData['use_config_notify_stock_qty'],
+                'notify_stock_qty' => $stockData['notify_stock_qty'],
+            ];
+
+            $this->sourceItemsConfigurationProcessor->process($sku, [$sourceItemData]);
+        }
+    }
+
+    /**
+     * @param array $sources
+     * @param string $sku
+     *
+     * @return void
+     */
+    private function processSourceItemsConfigurationInMultiSourceMode(array $sources, string $sku): void
+    {
         $assignedSources = isset($sources['assigned_sources']) && is_array($sources['assigned_sources'])
             ? $sources['assigned_sources']
             : [];
 
-        $this->sourceItemsConfigurationProcessor->process($product->getSku(), $assignedSources);
+        $this->sourceItemsConfigurationProcessor->process($sku, $assignedSources);
     }
 }
