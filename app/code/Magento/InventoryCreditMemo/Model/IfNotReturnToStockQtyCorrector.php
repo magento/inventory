4b88dcceb4a062e485f6de1d999a7e5bec03f03e
RomanKis <romaikiss@gmail.com>
2018-04-30 16:51:29 +0300

    MSI-676: Credit Memo: return product to stock

--- app/code/Magento/InventoryCreditMemo/Model/IfNotReturnToStockQtyCorrector.php
+++ app/code/Magento/InventoryCreditMemo/Model/IfNotReturnToStockQtyCorrector.php
@@ -8,16 +8,13 @@ declare(strict_types=1);
 namespace Magento\InventoryCreditMemo\Model;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\InventoryApi\Api\Data\SourceItemInterface;
-use Magento\InventoryApi\Api\SourceItemRepositoryInterface;
 use Magento\InventoryApi\Api\SourceItemsSaveInterface;
 use Magento\InventoryCatalog\Model\GetProductTypesBySkusInterface;
 use Magento\InventoryConfiguration\Model\IsSourceItemsAllowedForProductTypeInterface;
 use Magento\InventoryConfigurationApi\Api\GetStockItemConfigurationInterface;
-use Magento\InventoryReservationsApi\Api\ReservationBuilderInterface;
 use Magento\InventoryReservationsApi\Api\AppendReservationsInterface;
+use Magento\InventoryReservationsApi\Api\ReservationBuilderInterface;
 use Magento\InventorySales\Model\StockByWebsiteIdResolver;
 use Magento\Store\Model\StoreManagerInterface;
 
@@ -58,19 +55,9 @@ class IfNotReturnToStockQtyCorrector
     private $getProductTypesBySkus;
 
     /**
-     * @var SourceItemRepositoryInterface
-     */
-    private $sourceItemRepository;
-
-    /**
      * @var SourceItemsSaveInterface
      */
     private $sourceItemsSave;
-
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    private $searchCriteriaBuilder;
 
     /**
      * @var ProductRepositoryInterface
@@ -83,17 +70,21 @@ class IfNotReturnToStockQtyCorrector
     private $getStockItemConfiguration;
 
     /**
+     * @var GetSourceItem
+     */
+    private $getSourceItem;
+
+    /**
      * @param ReservationBuilderInterface $reservationBuilder
      * @param StockByWebsiteIdResolver $stockByWebsiteIdResolver
      * @param AppendReservationsInterface $appendReservations
      * @param StoreManagerInterface $storeManager
      * @param IsSourceItemsAllowedForProductTypeInterface $isSourceItemsAllowedForProductType
      * @param GetProductTypesBySkusInterface $getProductTypesBySkus
-     * @param SourceItemRepositoryInterface $sourceItemRepositoryInterface
      * @param SourceItemsSaveInterface $sourceItemsSave
-     * @param SearchCriteriaBuilder $searchCriteriaBuilder
      * @param ProductRepositoryInterface $productRepository
      * @param GetStockItemConfigurationInterface $getStockItemConfiguration
+     * @param GetSourceItem $getSourceItem
      */
     public function __construct(
         ReservationBuilderInterface $reservationBuilder,
@@ -102,11 +93,10 @@ class IfNotReturnToStockQtyCorrector
         StoreManagerInterface $storeManager,
         IsSourceItemsAllowedForProductTypeInterface $isSourceItemsAllowedForProductType,
         GetProductTypesBySkusInterface $getProductTypesBySkus,
-        SourceItemRepositoryInterface $sourceItemRepositoryInterface,
         SourceItemsSaveInterface $sourceItemsSave,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
         ProductRepositoryInterface $productRepository,
-        GetStockItemConfigurationInterface $getStockItemConfiguration
+        GetStockItemConfigurationInterface $getStockItemConfiguration,
+        GetSourceItem $getSourceItem
     ) {
         $this->reservationBuilder = $reservationBuilder;
         $this->stockByWebsiteIdResolver = $stockByWebsiteIdResolver;
@@ -114,11 +104,10 @@ class IfNotReturnToStockQtyCorrector
         $this->storeManager = $storeManager;
         $this->isSourceItemsAllowedForProductType = $isSourceItemsAllowedForProductType;
         $this->getProductTypesBySkus = $getProductTypesBySkus;
-        $this->sourceItemRepository = $sourceItemRepositoryInterface;
         $this->sourceItemsSave = $sourceItemsSave;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->productRepository = $productRepository;
         $this->getStockItemConfiguration = $getStockItemConfiguration;
+        $this->getSourceItem = $getSourceItem;
     }
 
     /**
@@ -126,7 +115,6 @@ class IfNotReturnToStockQtyCorrector
      * @param string $sourceCode
      * @param int $stockId
      * @return void
-     *
      * @throws LocalizedException
      */
     public function execute(array $items, string $sourceCode, int $stockId): void
@@ -151,7 +139,7 @@ class IfNotReturnToStockQtyCorrector
                     continue;
                 }
 
-                $sourceItem = $this->getSourceItem($sku, $sourceCode);
+                $sourceItem = $this->getSourceItem->execute($sku, $sourceCode);
 
                 if (($sourceItem->getQuantity() - $qty) >= 0) {
                     $sourceItem->setQuantity($sourceItem->getQuantity() - $qty);
@@ -173,23 +161,5 @@ class IfNotReturnToStockQtyCorrector
         if ($sourceItemsToSave) {
             $this->sourceItemsSave->execute($sourceItemsToSave);
         }
-    }
-
-    /**
-     * @param string $sku
-     * @param string $sourceCode
-     *
-     * @return SourceItemInterface
-     */
-    private function getSourceItem(string $sku, string $sourceCode): SourceItemInterface
-    {
-        $searchCriteria = $this->searchCriteriaBuilder
-            ->addFilter(SourceItemInterface::SKU, $sku)
-            ->addFilter(SourceItemInterface::SOURCE_CODE, $sourceCode)
-            ->create();
-        $sourceItems = $this->sourceItemRepository->getList($searchCriteria)->getItems();
-        $sourceItem = reset($sourceItems);
-
-        return $sourceItem;
     }
 }
