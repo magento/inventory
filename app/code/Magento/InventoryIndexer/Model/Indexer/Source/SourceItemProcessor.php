623aef64f2746e3ab7edf146e4f0cba92186ee36
Tadeu Rodrigues <karlostadeu85@gmail.com>
2019-02-03 02:38:28 -0200

    fix strict_types

--- app/code/Magento/InventoryIndexer/Model/Indexer/Source/SourceItemProcessor.php
+++ app/code/Magento/InventoryIndexer/Model/Indexer/Source/SourceItemProcessor.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\InventoryIndexer\Model\Indexer\Source;
 
 use Magento\Framework\Indexer\AbstractProcessor;
