4e0daf5500f2029d617e8df9afcd6553b1c2f612
al.kravchuk <al.kravchuk@ism-ukraine.com>
2019-03-14 11:03:09 +0200

    MSI-2030-Add an ability to Mark Orders placed with Store Pickup. Add new extension attribute.

---
+++ InventoryInStorePickup/Plugin/Sales/Order/GetPickupPointForOrderPlugin.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickup\Plugin\Sales\Order;
+
+use Magento\InventoryInStorePickup\Model\ResourceModel\OrderPickupPoint\GetPickupPointByOrderId;
+use Magento\Sales\Api\Data\OrderExtensionFactory;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+
+/**
+ * Class GetPickupPointForOrderPlugin.
+ * Set Pickup Point Identifier to Order Entity.
+ */
+class GetPickupPointForOrderPlugin
+{
+    /**
+     * @var OrderExtensionFactory
+     */
+    private $orderExtensionFactory;
+
+    /**
+     * @var GetPickupPointByOrderId
+     */
+    private $getPickupPointByOrderId;
+
+    /**
+     * GetPickupPointForOrderPlugin constructor.
+     *
+     * @param OrderExtensionFactory   $orderExtensionFactory
+     * @param GetPickupPointByOrderId $getPickupPointByOrderId
+     */
+    public function __construct(
+        OrderExtensionFactory $orderExtensionFactory,
+        GetPickupPointByOrderId $getPickupPointByOrderId
+    ) {
+        $this->orderExtensionFactory = $orderExtensionFactory;
+        $this->getPickupPointByOrderId = $getPickupPointByOrderId;
+    }
+
+    /**
+     * @param OrderRepositoryInterface $orderRepository
+     * @param OrderInterface           $order
+     *
+     * @return OrderInterface
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterGet(
+        OrderRepositoryInterface $orderRepository,
+        OrderInterface $order
+    ):OrderInterface {
+        $extension = $order->getExtensionAttributes();
+
+        if (empty($extension)) {
+            $extension = $this->orderExtensionFactory->create();
+        }
+
+        if ($extension->getPickupPointId()) {
+            return $order;
+        }
+
+        $pickupPointId = $this->getPickupPointByOrderId->execute((int)$order->getEntityId());
+
+        if ($pickupPointId) {
+            $extension->setPickupPointId($pickupPointId);
+        }
+
+        $order->setExtensionAttributes($extension);
+
+        return $order;
+    }
+}
---
+++ InventoryInStorePickup/Plugin/Sales/Order/SavePickupPointForOrderPlugin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickup\Plugin\Sales\Order;
+
+use Magento\InventoryInStorePickup\Model\ResourceModel\OrderPickupPoint\SaveOrderPickupPoint;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+
+class SavePickupPointForOrderPlugin
+{
+    /**
+     * @var SaveOrderPickupPoint
+     */
+    private $saveOrderPickupPoint;
+
+    /**
+     * SavePickupPointForOrderPlugin constructor.
+     *
+     * @param SaveOrderPickupPoint $saveOrderPickupPoint
+     */
+    public function __construct(SaveOrderPickupPoint $saveOrderPickupPoint)
+    {
+        $this->saveOrderPickupPoint = $saveOrderPickupPoint;
+    }
+
+    /**
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+     * @param \Magento\Sales\Api\Data\OrderInterface      $result
+     * @param \Magento\Sales\Api\Data\OrderInterface      $entity
+     *
+     * @return \Magento\Sales\Api\Data\OrderInterface
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterSave(
+        OrderRepositoryInterface $orderRepository,
+        OrderInterface $result,
+        OrderInterface $entity
+    ) {
+        $extension = $result->getExtensionAttributes();
+
+        if (!empty($extension) && $extension->getPickupPointId()) {
+            $this->saveOrderPickupPoint->execute((int)$result->getEntityId(), $extension->getPickupPointId());
+        }
+
+        return $result;
+    }
+}
