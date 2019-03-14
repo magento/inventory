95e787800186f7fcfac144e9f2d3753f8fab4a68
al.kravchuk <al.kravchuk@ism-ukraine.com>
2019-03-14 11:03:09 +0200

    MSI-2030-Add an ability to Mark Orders placed with Store Pickup. Add new extension attribute.

---
+++ app/code/Magento/InventoryInStorePickup/Model/ResourceModel/OrderPickupPoint/GetPickupPointByOrderId.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickup\Model\ResourceModel\OrderPickupPoint;
+
+use Magento\Framework\App\ResourceConnection;
+
+/**
+ * Class GetPickupPointByOrderId.
+ * Get Pickup Point identifier by order identifier.
+ */
+class GetPickupPointByOrderId
+{
+    const ORDER_ID        = 'order_id';
+    const PICKUP_POINT_ID = 'source_code';
+
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    private $connection;
+
+    /**
+     * GetPickupPointByOrderId constructor.
+     *
+     * @param \Magento\Framework\App\ResourceConnection $connection
+     */
+    public function __construct(
+        ResourceConnection $connection
+    ) {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Fetch pickup point identifier by order identifier.
+     *
+     * @param int $orderId
+     *
+     * @return string|null
+     */
+    public function execute(int $orderId):?string
+    {
+        $connection = $this->connection->getConnection();
+        $table = $this->connection->getTableName('inventory_pickup_point_order');
+
+        $select = $connection->select()
+             ->from($table, [
+                     self::PICKUP_POINT_ID => self::PICKUP_POINT_ID
+             ])
+             ->where(self::ORDER_ID . '= ?', $orderId)
+             ->limit(1);
+
+        $id = $connection->fetchOne($select);
+
+        return $id ?: null;
+    }
+}
---
+++ app/code/Magento/InventoryInStorePickup/Model/ResourceModel/OrderPickupPoint/SaveOrderPickupPoint.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickup\Model\ResourceModel\OrderPickupPoint;
+
+use Magento\Framework\App\ResourceConnection;
+
+/**
+ * Class SaveOrderPickupPoint.
+ * Save Order Pickup Point
+ */
+class SaveOrderPickupPoint
+{
+    const ORDER_ID        = 'order_id';
+    const PICKUP_POINT_ID = 'source_code';
+
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    private $connection;
+
+    /**
+     * GetPickupPointByOrderId constructor.
+     *
+     * @param \Magento\Framework\App\ResourceConnection $connection
+     */
+    public function __construct(
+        ResourceConnection $connection
+    ) {
+        $this->connection = $connection;
+    }
+
+    /**
+     * Fetch pickup point identifier by order identifier.
+     *
+     * @param int $orderId
+     * @param string $pickupPointId
+     *
+     * @return void
+     */
+    public function execute(int $orderId, string $pickupPointId):void
+    {
+        $connection = $this->connection->getConnection();
+        $table = $this->connection->getTableName('inventory_pickup_point_order');
+
+        $data = [
+            self::ORDER_ID => $orderId,
+            self::PICKUP_POINT_ID => $pickupPointId
+        ];
+
+        $connection->insertOnDuplicate($table, $data);
+    }
+}
