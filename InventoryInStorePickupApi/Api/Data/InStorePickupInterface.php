2f87a2e0395ba5d5887dd2d04374065892721a81
Ievgen Shakhsuvarov <ishakhsuvarov@magento.com>
2019-03-25 16:45:49 -0500

    magento-engcom/msi#2044: Static tests adjusted

--- InventoryInStorePickupApi/Api/Data/InStorePickupInterface.php
+++ InventoryInStorePickupApi/Api/Data/InStorePickupInterface.php
@@ -7,10 +7,32 @@ declare(strict_types=1);
 
 namespace Magento\InventoryInStorePickupApi\Api\Data;
 
+/**
+ * This class may require refactoring.
+ *
+ * @deprecated
+ */
 interface InStorePickupInterface extends \Magento\Framework\Api\ExtensibleDataInterface
 {
     /**
      * Constants for keys of data array. Identical to the name of the getter in snake case
      */
     const IN_STORE_PICKUP_CODE = 'in_store_pickup';
+
+    /**
+     * Retrieve existing extension attributes object or create a new one.
+     *
+     * @return \Magento\InventoryInStorePickupApi\Api\Data\InStorePickupExtensionInterface|null
+     */
+    public function getExtensionAttributes();
+
+    /**
+     * Set an extension attributes object.
+     *
+     * @param \Magento\InventoryInStorePickupApi\Api\Data\InStorePickupExtensionInterface $extensionAttributes
+     * @return $this
+     */
+    public function setExtensionAttributes(
+        \Magento\InventoryInStorePickupApi\Api\Data\InStorePickupExtensionInterface $extensionAttributes
+    );
 }
