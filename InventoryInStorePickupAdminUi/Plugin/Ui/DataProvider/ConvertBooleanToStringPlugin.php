9653126c4e32f5ba3c1d2ec094cfc327f7c9b02b
al.kravchuk <al.kravchuk@ism-ukraine.com>
2019-03-31 11:28:02 +0300

    MSI#2116 - Improve existing implementation of "In Store Pickup" attribute of Source. Apply changes according to code review notices.

--- InventoryInStorePickupAdminUi/Plugin/Ui/DataProvider/ConvertBooleanToStringPlugin.php
+++ InventoryInStorePickupAdminUi/Plugin/Ui/DataProvider/ConvertBooleanToStringPlugin.php
@@ -10,11 +10,15 @@ namespace Magento\InventoryInStorePickupAdminUi\Plugin\Ui\DataProvider;
 use Magento\Framework\Api\ExtensibleDataInterface;
 use Magento\InventoryAdminUi\Ui\DataProvider\SourceDataProvider;
 
+/**
+ * @deprecated Need to be removed when issue will be resolved in core.
+ * @see Please check issue in core for more details: https://github.com/magento/magento2/issues/22067.
+ */
 class ConvertBooleanToStringPlugin
 {
     /**
      * Convert the extension attribute boolean (true|false) to string integer value ("1"|"0") to match expected type.
-     * Ui DataProvider does not support this for Extension Attributes   .
+     * Ui DataProvider does not support this for Extension Attributes.
      * @see \Magento\Ui\DataProvider\SearchResultFactory::createAttributes
      *
      * @param SourceDataProvider $subject
@@ -28,13 +32,13 @@ class ConvertBooleanToStringPlugin
     ): array {
         if (array_key_exists('items', $result)) {
             foreach ($result['items'] as $key => $item) {
-                $result['items'][$key] = $this->convertBooleanToString($item);
+                $result['items'][$key] = $this->convertDataItemExtensionFieldsBoolValueToString($item);
             }
         } else {
             // Single attribute returned in:
             // \Magento\InventoryAdminUi\Ui\DataProvider\SourceDataProvider::getData
             foreach ($result as $key => $item) {
-                $result[$key]['general'] = $this->convertBooleanToString($item['general']);
+                $result[$key]['general'] = $this->convertDataItemExtensionFieldsBoolValueToString($item['general']);
             }
         }
 
@@ -48,7 +52,7 @@ class ConvertBooleanToStringPlugin
      *
      * @return array
      */
-    private function convertBooleanToString(array $item):array
+    private function convertDataItemExtensionFieldsBoolValueToString(array $item):array
     {
         if (isset($item[ExtensibleDataInterface::EXTENSION_ATTRIBUTES_KEY])) {
             foreach ($item[ExtensibleDataInterface::EXTENSION_ATTRIBUTES_KEY] as $code => $value) {
