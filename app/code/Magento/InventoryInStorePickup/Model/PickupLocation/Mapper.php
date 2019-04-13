c7347f540f5e8cd1f323eb9bc04f4fa366f47c9a
al.kravchuk <al.kravchuk@ism-ukraine.com>
2019-04-13 16:35:21 +0300

    MSI-2126 Immutable DTO for Pickup Location. Add Pickup Location and Mapper for Source -> Pickup Location projection.

---
+++ app/code/Magento/InventoryInStorePickup/Model/PickupLocation/Mapper.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickup\Model\PickupLocation;
+
+use Magento\InventoryApi\Api\Data\SourceInterface;
+use Magento\InventoryInStorePickup\Model\PickupLocation\Mapper\CreateFromSource;
+use Magento\InventoryInStorePickupApi\Api\Data\PickupLocationInterface;
+
+class Mapper
+{
+    /**
+     * Attributes map for projection.
+     *
+     * @var array
+     */
+    private $map;
+
+    /**
+     * @var \Magento\InventoryInStorePickup\Model\PickupLocation\Mapper\CreateFromSource
+     */
+    private $createFromSource;
+
+    /**
+     * @param \Magento\InventoryInStorePickup\Model\PickupLocation\Mapper\CreateFromSource $createFromSource
+     * @param array $map
+     */
+    public function __construct(
+        CreateFromSource $createFromSource,
+        array $map = []
+    ) {
+        $this->map = $map;
+        $this->createFromSource = $createFromSource;
+    }
+
+    /**
+     * @param \Magento\InventoryApi\Api\Data\SourceInterface $source
+     *
+     * @return \Magento\InventoryInStorePickupApi\Api\Data\PickupLocationInterface
+     */
+    public function map(SourceInterface $source): PickupLocationInterface
+    {
+        return $this->createFromSource->execute($source, $this->map);
+    }
+}
