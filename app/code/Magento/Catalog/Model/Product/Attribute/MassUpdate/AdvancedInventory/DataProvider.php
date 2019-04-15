062a64aff39181634db1d0fe7cdee29d9272f2d7
Francesco Tobia <francesco.tobia@magespecialist.it>
2019-04-15 20:18:24 +0200

    PHTML implementation of Advanced Inventory tab in product mass update refactored using UI components
    Issue: #2138

---
+++ app/code/Magento/Catalog/Model/Product/Attribute/MassUpdate/AdvancedInventory/DataProvider.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Catalog\Model\Product\Attribute\MassUpdate\AdvancedInventory;
+
+use Magento\Catalog\Model\ResourceModel\Product\Attribute\CollectionFactory;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+
+/**
+ * Data provider for the form of adding new product attribute.
+ */
+class DataProvider extends AbstractDataProvider
+{
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $collectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        return [];
+    }
+}
