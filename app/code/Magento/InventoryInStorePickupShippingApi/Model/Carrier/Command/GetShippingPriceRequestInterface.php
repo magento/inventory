d7133c40a0fe7dc27cbf37153b9b6337725c591b
al.kravchuk <al.kravchuk@ism-ukraine.com>
2019-05-29 21:29:50 +0300

    MSI-2083 Create 'In Store Pickup' Shipping Method. Rename Checkout -> Shipping.

---
+++ app/code/Magento/InventoryInStorePickupShippingApi/Model/Carrier/Command/GetShippingPriceRequestInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickupShippingApi\Model\Carrier\Command;
+
+use Magento\InventoryInStorePickupShippingApi\Api\Data\ShippingPriceRequestExtensionInterface;
+use Magento\InventoryInStorePickupShippingApi\Api\Data\ShippingPriceRequestInterface;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+
+/**
+ * Create Shipping Price Request Data Transfer Object.
+ *
+ * @api
+ */
+interface GetShippingPriceRequestInterface
+{
+    /**
+     * @param RateRequest $rateRequest
+     * @param float $defaultPrice
+     * @param float $freePackages
+     *
+     * @param ShippingPriceRequestExtensionInterface|null $shippingPriceRequestExtension
+     *
+     * @return ShippingPriceRequestInterface
+     */
+    public function execute(
+        RateRequest $rateRequest,
+        float $defaultPrice,
+        float $freePackages,
+        ?ShippingPriceRequestExtensionInterface $shippingPriceRequestExtension = null
+    ): ShippingPriceRequestInterface;
+}
