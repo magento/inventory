567960836cc2bad493a1f380f63dcd50c9f7beff
al.kravchuk <al.kravchuk@ism-ukraine.com>
2019-06-10 23:15:38 +0300

    MSI-2286-In-Store-Pickup-checkout-implementation-on-Weabpi-level. Add save and load of pickup location exetension attribute for Quote Address entity.

---
+++ InventoryInStorePickupQuote/Plugin/Quote/Address/SavePickupLocationForQuoteAddress.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickupQuote\Plugin\Quote\Address;
+
+use Magento\InventoryInStorePickupQuote\Model\ResourceModel\SaveQuoteAddressPickupLocation;
+use Magento\Quote\Api\Data\AddressInterface;
+use Magento\Quote\Model\ResourceModel\Quote\Address;
+
+/**
+ * Save selected Pickup Location Code for Quote Address.
+ */
+class SavePickupLocationForQuoteAddress
+{
+    /**
+     * @var SaveQuoteAddressPickupLocation
+     */
+    private $saveQuoteAddressPickupLocation;
+
+    /**
+     * SavePickupLocationForQuoteAddress constructor.
+     *
+     * @param SaveQuoteAddressPickupLocation $saveQuoteAddressPickupLocation
+     */
+    public function __construct(SaveQuoteAddressPickupLocation $saveQuoteAddressPickupLocation)
+    {
+        $this->saveQuoteAddressPickupLocation = $saveQuoteAddressPickupLocation;
+    }
+
+    /**
+     * @param Address $subject
+     * @param Address $result
+     * @param AddressInterface $entity
+     *
+     * @return Address
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterSave(Address $subject, Address $result, AddressInterface $entity): Address
+    {
+        if ($entity->getExtensionAttributes() && $entity->getExtensionAttributes()->getPickupLocationCode()) {
+            $this->saveQuoteAddressPickupLocation->execute(
+                (int)$entity->getId(),
+                $entity->getExtensionAttributes()->getPickupLocationCode()
+            );
+        }
+
+        return $result;
+    }
+}
---
+++ InventoryInStorePickupQuote/Plugin/Quote/AddressCollection/GetPickupLocationInformationPlugin.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickupQuote\Plugin\Quote\AddressCollection;
+
+use Magento\Quote\Api\Data\AddressExtensionInterfaceFactory;
+use Magento\Quote\Model\Quote\Address;
+use Magento\Quote\Model\ResourceModel\Quote\Address\Collection;
+
+/**
+ * Load Pickup Location and add to Extension Attributes.
+ */
+class GetPickupLocationInformationPlugin
+{
+    private const PICKUP_LOCATION_CODE = 'pickup_location_code';
+    private const TABLE_ALIAS = 'iplqa';
+
+    /**
+     * @var AddressExtensionInterfaceFactory
+     */
+    private $addressExtensionInterfaceFactory;
+
+    /**
+     * GetPickupLocationInformation constructor.
+     *
+     * @param AddressExtensionInterfaceFactory $addressExtensionInterfaceFactory
+     */
+    public function __construct(AddressExtensionInterfaceFactory $addressExtensionInterfaceFactory)
+    {
+        $this->addressExtensionInterfaceFactory = $addressExtensionInterfaceFactory;
+    }
+
+    /**
+     * @param Collection $collection
+     * @param \Closure $proceed
+     *
+     * @return Collection
+     * @throws \Zend_Db_Select_Exception
+     */
+    public function aroundLoadWithFilter(Collection $collection, \Closure $proceed): Collection
+    {
+        if ($collection->isLoaded()) {
+            return $proceed();
+        }
+
+        if (!isset($collection->getSelect()->getPart(\Zend_Db_Select::FROM)[self::TABLE_ALIAS])) {
+            $collection->getSelect()->joinLeft(
+                [self::TABLE_ALIAS => 'inventory_pickup_location_quote_address'],
+                self::TABLE_ALIAS . '.address_id = main_table.address_id',
+                [self::PICKUP_LOCATION_CODE]
+            );
+        }
+
+        $result = $proceed();
+
+        /** @var Address $address */
+        foreach ($collection->getItems() as $address) {
+            if ($address->hasData(self::PICKUP_LOCATION_CODE)) {
+                $this->addPickupLocationToExtensionAttributes($address);
+            }
+            $address->unsetData(self::PICKUP_LOCATION_CODE);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Add Loaded Pickup Location to Extension Attributes.
+     *
+     * @param Address $item
+     */
+    private function addPickupLocationToExtensionAttributes(Address $item): void
+    {
+        if (!$item->getExtensionAttributes()) {
+            $item->setExtensionAttributes($this->addressExtensionInterfaceFactory->create());
+        }
+
+        $item->getExtensionAttributes()->setPickupLocationCode($item->getData('pickup_location_code'));
+    }
+}
