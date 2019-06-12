89b14856ba577fd03c5961834f7e8bbeb509aaee
Maksym Novik <m.novik@ism-ukraine.com>
2019-06-13 00:18:19 +0300

    Simplified work with extension attributes a little bit.

---
+++ app/code/Magento/InventoryInStorePickup/Model/Source/GetIsPickupLocationActive.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickup\Model\Source;
+
+use Magento\InventoryApi\Api\Data\SourceInterface;
+
+/**
+ * Anti copy-paste service.
+ * Extracts is_pickup_location_active from the source with all the null-checks
+ */
+class GetIsPickupLocationActive
+{
+    /**
+     * @param SourceInterface $source
+     *
+     * @return bool
+     */
+    public function execute(SourceInterface $source): bool
+    {
+        $extension = $source->getExtensionAttributes();
+        if ($extension) {
+            return (bool)$extension->getIsPickupLocationActive();
+        }
+
+        return false;
+    }
+}
