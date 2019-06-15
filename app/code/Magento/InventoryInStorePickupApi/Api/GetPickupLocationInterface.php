2c79b4c663eb1fc34129761b94dad599fbb43086
al.kravchuk <al.kravchuk@ism-ukraine.com>
2019-06-15 16:42:39 +0300

    MSI-2286-In-Store-Pickup-checkout-implementation-on-Weabpi-level. Add service to provide Pickup Location by Source Code, Sales Channel and Sales Channel Type.

---
+++ app/code/Magento/InventoryInStorePickupApi/Api/GetPickupLocationInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickupApi\Api;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use \Magento\InventoryInStorePickupApi\Api\Data\PickupLocationInterface;
+
+/**
+ * Get Pickup Location by provided Pickup Location Code.
+ *
+ * @api
+ */
+interface GetPickupLocationInterface
+{
+    /**
+     * @param string $pickupLocationCode
+     *
+     * @param string $salesChannelType
+     * @param string $salesChannelCode
+     *
+     * @return PickupLocationInterface
+     * @throws NoSuchEntityException
+     */
+    public function execute(
+        string $pickupLocationCode,
+        string $salesChannelType,
+        string $salesChannelCode
+    ): PickupLocationInterface;
+}
