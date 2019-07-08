d5ff3adac682eebe654d046bb9320f52eacd7181
Mastiuhin Olexandr <mastiuhin.olexandr@transoftgroup.com>
2019-07-08 17:31:24 +0300

    MC-17524: Child product is not addable to Cart from PLP if it does not have default source assigned

--- app/code/Magento/InventoryApi/Test/_files/change_stock_for_current_website_rollback.php
+++ app/code/Magento/InventoryApi/Test/_files/change_stock_for_current_website_rollback.php
@@ -9,19 +9,29 @@ use Magento\TestFramework\Helper\Bootstrap;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterfaceFactory;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
 use Magento\Store\Model\StoreManagerInterface;
-
-require __DIR__ . '/stock_with_source.php';
+use Magento\InventoryApi\Api\StockRepositoryInterface;
+use Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface;
 
 $objectManager = Bootstrap::getObjectManager();
 /** @var SalesChannelInterfaceFactory $salesChannelFactory */
 $salesChannelFactory = $objectManager->get(SalesChannelInterfaceFactory::class);
 /** @var StoreManagerInterface $storeManager */
 $storeManager = $objectManager->get(StoreManagerInterface::class);
+/** @var DefaultStockProviderInterface $defaultStockProvider */
+$defaultStockProvider = $objectManager->get(DefaultStockProviderInterface::class);
+/** @var StockRepositoryInterface $stockRepository */
+$stockRepository = $objectManager->get(StockRepositoryInterface::class);
 
-$extensionAttributes = $stock->getExtensionAttributes();
+/**
+ * Change back default stock to use current website.
+ */
+$defaultStock = $stockRepository->get($defaultStockProvider->getId());
+$extensionAttributes = $defaultStock->getExtensionAttributes();
 $salesChannel = $salesChannelFactory->create();
 $website = $storeManager->getWebsite();
 $salesChannel->setCode($website->getCode());
 $salesChannel->setType(SalesChannelInterface::TYPE_WEBSITE);
 $extensionAttributes->setSalesChannels([$salesChannel]);
-$stockRepository->save($stock);
+$stockRepository->save($defaultStock);
+
+require __DIR__ . '/stock_with_source_link_rollback.php';
