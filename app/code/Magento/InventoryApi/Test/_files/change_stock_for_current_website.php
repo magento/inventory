59400774cbcdce815c138cc974da1eaef7a66e19
Mastiuhin Olexandr <mastiuhin.olexandr@transoftgroup.com>
2019-07-09 13:03:13 +0300

    MC-17524: Child product is not addable to Cart from PLP if it does not have default source assigned

--- app/code/Magento/InventoryApi/Test/_files/change_stock_for_current_website.php
+++ app/code/Magento/InventoryApi/Test/_files/change_stock_for_current_website.php
@@ -8,20 +8,16 @@ declare(strict_types=1);
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterfaceFactory;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
-use Magento\Store\Model\StoreManagerInterface;
 
 require __DIR__ . '/stock_with_source_link.php';
 
 $objectManager = Bootstrap::getObjectManager();
 /** @var SalesChannelInterfaceFactory $salesChannelFactory */
 $salesChannelFactory = $objectManager->get(SalesChannelInterfaceFactory::class);
-/** @var StoreManagerInterface $storeManager */
-$storeManager = $objectManager->get(StoreManagerInterface::class);
 
 $extensionAttributes = $stock->getExtensionAttributes();
 $salesChannel = $salesChannelFactory->create();
-$website = $storeManager->getWebsite();
-$salesChannel->setCode($website->getCode());
+$salesChannel->setCode('base');
 $salesChannel->setType(SalesChannelInterface::TYPE_WEBSITE);
 $extensionAttributes->setSalesChannels([$salesChannel]);
 $stockRepository->save($stock);
