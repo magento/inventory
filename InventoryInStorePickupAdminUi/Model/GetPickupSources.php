71e3b945e93fe8bb756c72a47eba129ae44e56e1
Maksym Novik <m.novik@ism-ukraine.com>
2019-07-12 17:26:36 +0300

    MSI-2287 in store pickup orders placement from admin panel #2390. Some refactoring

--- InventoryInStorePickupAdminUi/Model/GetPickupSources.php
+++ InventoryInStorePickupAdminUi/Model/GetPickupSources.php
@@ -10,22 +10,14 @@ namespace Magento\InventoryInStorePickupAdminUi\Model;
 
 use Magento\Framework\Exception\InputException;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\InventoryApi\Api\GetSourcesAssignedToStockOrderedByPriorityInterface;
 use Magento\InventoryInStorePickup\Model\Source\GetIsPickupLocationActive;
-use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
-use Magento\InventorySalesApi\Api\StockResolverInterface;
 
 /**
  * Get list of sources marked as pickup location by website
  */
 class GetPickupSources
 {
-    /**
-     * @var StockResolverInterface
-     */
-    private $stockResolver;
-
     /**
      * @var GetSourcesAssignedToStockOrderedByPriorityInterface
      */
@@ -37,37 +29,28 @@ class GetPickupSources
     private $getIsPickupLocationActive;
 
     /**
-     * @param StockResolverInterface $stockResolver
      * @param GetSourcesAssignedToStockOrderedByPriorityInterface $getSourcesAssignedToStockOrderedByPriority
      * @param GetIsPickupLocationActive $getIsPickupLocationActive
      */
     public function __construct(
-        StockResolverInterface $stockResolver,
         GetSourcesAssignedToStockOrderedByPriorityInterface $getSourcesAssignedToStockOrderedByPriority,
         GetIsPickupLocationActive $getIsPickupLocationActive
     ) {
 
-        $this->stockResolver = $stockResolver;
         $this->getSourcesAssignedToStockOrderedByPriority = $getSourcesAssignedToStockOrderedByPriority;
         $this->getIsPickupLocationActive = $getIsPickupLocationActive;
     }
 
     /**
-     * @param string $websiteCode
+     * @param int $stockId
      *
      * @return array
      * @throws InputException
      * @throws LocalizedException
-     * @throws NoSuchEntityException
      */
-    public function execute(string $websiteCode): array
+    public function execute(int $stockId): array
     {
-        $stockSources = $this->getSourcesAssignedToStockOrderedByPriority->execute(
-            $this->stockResolver->execute(
-                SalesChannelInterface::TYPE_WEBSITE,
-                $websiteCode
-            )->getStockId()
-        );
+        $stockSources = $this->getSourcesAssignedToStockOrderedByPriority->execute($stockId);
 
         return array_filter($stockSources, [$this->getIsPickupLocationActive, 'execute']);
     }
--- InventoryInStorePickupAdminUi/ViewModel/CreateOrder/SourcesForm.php
+++ InventoryInStorePickupAdminUi/ViewModel/CreateOrder/SourcesForm.php
@@ -15,6 +15,8 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\InventoryApi\Api\Data\SourceInterface;
 use Magento\InventoryInStorePickupAdminUi\Model\GetPickupSources;
+use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
+use Magento\InventorySalesApi\Api\StockResolverInterface;
 
 /**
  * ViewModel for Magento_InventoryInStorePickupAdminUi::order/create/shipping/method/sources_form.phtml
@@ -35,27 +37,26 @@ class SourcesForm implements ArgumentInterface
     /**
      * @var array|null
      */
-    private $pickupLocationsArray;
+    private $pickupSources;
+
+    /**
+     * @var StockResolverInterface
+     */
+    private $stockResolver;
 
     /**
      * @param Quote $backendQuote
      * @param GetPickupSources $getPickupSources
+     * @param StockResolverInterface $stockResolver
      */
     public function __construct(
         Quote $backendQuote,
-        GetPickupSources $getPickupSources
+        GetPickupSources $getPickupSources,
+        StockResolverInterface $stockResolver
     ) {
         $this->backendQuote = $backendQuote;
         $this->getPickupSources = $getPickupSources;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getSelectedPickupLocationCode(): ?string
-    {
-        //TODO
-        return 'zzz';
+        $this->stockResolver = $stockResolver;
     }
 
     /**
@@ -65,13 +66,13 @@ class SourcesForm implements ArgumentInterface
      * @throws LocalizedException
      * @throws NoSuchEntityException
      */
-    public function getPickupLocationsArray(): array
+    public function getPickupSourcesOptionsList(): array
     {
-        if ($this->pickupLocationsArray === null) {
-            $this->initializePickupLocationsArray();
+        if ($this->pickupSources === null) {
+            $this->loadPickupSources();
         }
 
-        return $this->pickupLocationsArray;
+        return $this->pickupSources;
     }
 
     /**
@@ -79,14 +80,26 @@ class SourcesForm implements ArgumentInterface
      * @throws LocalizedException
      * @throws NoSuchEntityException
      */
-    private function initializePickupLocationsArray(): void
+    private function loadPickupSources(): void
     {
-        $pickupSources = $this->getPickupSources->execute($this->backendQuote->getStore()->getWebsite()->getCode());
+        $pickupSources = $this->getPickupSources->execute($this->getStockId());
 
-        $this->pickupLocationsArray = [];
+        $this->pickupSources = [];
         /** @var SourceInterface $source */
         foreach ($pickupSources as $source) {
-            $this->pickupLocationsArray[$source->getSourceCode()] = $source->getName();
+            $this->pickupSources[$source->getSourceCode()] = $source->getName();
         }
+    }
+
+    /**
+     * @return int|null
+     * @throws NoSuchEntityException
+     */
+    private function getStockId()
+    {
+        return $this->stockResolver->execute(
+            SalesChannelInterface::TYPE_WEBSITE,
+            $this->backendQuote->getStore()->getWebsite()->getCode()
+        )->getStockId();
     }
 }
