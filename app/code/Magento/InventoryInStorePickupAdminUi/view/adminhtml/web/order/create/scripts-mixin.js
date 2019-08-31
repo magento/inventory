9c9b06e86f848fb1dbb3f55fada23522f0d58733
Maksym Novik <maksym.novik@decimadigital.com>
2019-08-31 17:26:28 +0300

    MSI-2287 in store pickup orders placement from admin panel #2390. Hide sources form if store pickup is not selected

--- app/code/Magento/InventoryInStorePickupAdminUi/view/adminhtml/web/order/create/scripts-mixin.js
+++ app/code/Magento/InventoryInStorePickupAdminUi/view/adminhtml/web/order/create/scripts-mixin.js
@@ -10,29 +10,43 @@ define([
         'use strict';
         return function () {
             const STORE_PICKUP_METHOD = 'in_store_pickup';
+            const SOURCES_FIELD_SELECTOR = '#shipping_form_pickup_location_source';
 
             window.AdminOrder.prototype.setShippingMethod = function (method) {
                 let data = {};
 
                 data['order[shipping_method]'] = method;
+
                 this.loadArea([
                     'shipping_method',
                     'totals',
                     'billing_method'
-                ], true, data);
-
-                hideShippingAddressForm(method === STORE_PICKUP_METHOD) /* <-- That`s also a line with modifications */
+                ], true, data).then(
+                    function () {
+                        setStorePickupMethod(method === STORE_PICKUP_METHOD) /* <-- That`s a line with modifications */
+                    }
+                );
             };
 
-            function hideShippingAddressForm(hide) {
+            /**
+             * Disable billing address form;
+             * Display sources dropdown field;
+             * And vice-versa
+             *
+             * @param {boolean} isStorePickup
+             */
+            function setStorePickupMethod(isStorePickup) {
+                let sourcesInput = $(SOURCES_FIELD_SELECTOR);
                 let theSameAsBilling = $('#order-shipping_same_as_billing + label');
-                if (hide) {
+                if (isStorePickup) {
                     order.disableShippingAddress(true);
                     theSameAsBilling.hide();
+                    sourcesInput.show();
                     return;
                 }
                 order.disableShippingAddress($('#order-shipping_same_as_billing').prop('checked'));
                 theSameAsBilling.show();
+                sourcesInput.hide();
             }
         };
     });
