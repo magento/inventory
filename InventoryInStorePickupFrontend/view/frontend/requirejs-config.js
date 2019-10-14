459ae91e9b891fedd5737184677ccd3493265424
Stanislav Idolov <sidolov@magento.com>
2019-10-14 13:21:23 -0500

    magento/inventory#2280: Removed rates validator mixin to enable proper tax calculation

--- InventoryInStorePickupFrontend/view/frontend/requirejs-config.js
+++ InventoryInStorePickupFrontend/view/frontend/requirejs-config.js
@@ -4,9 +4,6 @@ var config = {
             'Magento_Checkout/js/model/quote': {
                 'Magento_InventoryInStorePickupFrontend/js/model/quote-ext': true,
             },
-            'Magento_Checkout/js/model/shipping-rates-validator': {
-                'Magento_InventoryInStorePickupFrontend/js/model/shipping-rates-validator-ext': true,
-            },
         },
     },
 };
