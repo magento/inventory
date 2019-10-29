a7b7252f1d1cb34cdaa3653a59d51551d1e4116f
Stanislav Idolov <sidolov@magento.com>
2019-10-29 16:16:12 -0500

    magento/inventory#2669: Fixed minor issues

--- InventoryInStorePickupShippingAdminUi/Plugin/Shipping/Block/Adminhtml/View/ShipmentTrackingButtonPlugin.php
+++ InventoryInStorePickupShippingAdminUi/Plugin/Shipping/Block/Adminhtml/View/ShipmentTrackingButtonPlugin.php
@@ -9,6 +9,7 @@ namespace Magento\InventoryInStorePickupShippingAdminUi\Plugin\Shipping\Block\Ad
 
 use Magento\InventoryInStorePickupShippingApi\Model\Carrier\InStorePickup;
 use Magento\Shipping\Block\Adminhtml\View;
+use Magento\Framework\View\LayoutInterface;
 
 /**
  * 'Send tracking information' button processor.
@@ -19,9 +20,11 @@ class ShipmentTrackingButtonPlugin
      * Remove 'Send tracking information' button in case shipping method is store pickup.
      *
      * @param View $subject
+     * @param LayoutInterface $layout
      * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function beforeSetLayout(View $subject): void
+    public function beforeSetLayout(View $subject, LayoutInterface $layout): void
     {
         if ($subject->getShipment()->getOrder()->getShippingMethod() === InStorePickup::DELIVERY_METHOD) {
             $subject->removeButton('save');
