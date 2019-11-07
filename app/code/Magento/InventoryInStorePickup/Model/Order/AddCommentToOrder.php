43c6d9e1ba2eca948d175df00d753813d9deeec4
Stanislav Idolov <sidolov@magento.com>
2019-11-07 11:05:24 -0600

    magento/inventory#2683: Tests stabilization

---
+++ app/code/Magento/InventoryInStorePickup/Model/Order/AddCommentToOrder.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\InventoryInStorePickup\Model\Order;
+
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+
+class AddCommentToOrder
+{
+    /**
+     * @var TimezoneInterface
+     */
+    private $timezone;
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * AddCommentToOrder constructor.
+     * @param OrderRepositoryInterface $orderRepository
+     * @param TimezoneInterface $timezone
+     */
+    public function __construct(
+        OrderRepositoryInterface $orderRepository,
+        TimezoneInterface $timezone
+    ) {
+        $this->orderRepository = $orderRepository;
+        $this->timezone = $timezone;
+    }
+    /**
+     * @param OrderInterface $order
+     * @throws \Exception
+     * @return void
+     */
+    public function execute(OrderInterface $order) : void
+    {
+        $time = $this->timezone->formatDateTime(
+            new \DateTime(),
+            \IntlDateFormatter::LONG,
+            \IntlDateFormatter::MEDIUM
+        );
+        $comment = __('Order notified for pickup at: %1',  $time);
+        $order->addCommentToStatusHistory($comment, $order->getStatus(), true)->setIsCustomerNotified(1);
+        $this->orderRepository->save($order);
+    }
+}
