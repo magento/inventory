7c701851b074e059121b53e6962f6bbf271b4854
Vladyslav Podorozhnyi <vpodorozh@gmail.com>
2019-11-13 14:49:06 +0200

    magento/inventory: #2586 Place In-Store Pickup Order with Webapi tests coverage #2586

--- app/code/Magento/InventoryInStorePickup/Test/Api/OrderPlacementBase.php
+++ app/code/Magento/InventoryInStorePickup/Test/Api/OrderPlacementBase.php
@@ -115,7 +115,10 @@ abstract class OrderPlacementBase extends OrderPlacementBaseSales
 
         $body = [
             'addressInformation' => [
-                'shipping_address' => array_merge($addressData, ['extension_attributes' => ['pickup_location_code' => 'eu-1']]),
+                'shipping_address' => array_merge(
+                    $addressData,
+                    ['extension_attributes' => ['pickup_location_code' => 'eu-1']]
+                ),
                 'billing_address' => $addressData,
                 'shipping_carrier_code' => 'flatrate',
                 'shipping_method_code' => 'flatrate'
