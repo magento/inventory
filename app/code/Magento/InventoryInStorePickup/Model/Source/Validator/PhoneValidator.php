f94fa101f1954e9ea9747ea504539e6a12f98920
Oleksandr Kravchuk <swnsma@gmail.com>
2019-11-14 17:11:11 +0200

    Update app/code/Magento/InventoryInStorePickup/Model/Source/Validator/PhoneValidator.php
    Co-Authored-By: Maksym Novik <novik.kor@gmail.com>

--- app/code/Magento/InventoryInStorePickup/Model/Source/Validator/PhoneValidator.php
+++ app/code/Magento/InventoryInStorePickup/Model/Source/Validator/PhoneValidator.php
@@ -48,7 +48,7 @@ class PhoneValidator implements SourceValidatorInterface
         $value = (string)$source->getCity();
         $errors = [];
 
-        if ($this->getIsPickupLocationActive->execute($source) && '' === trim($value)) {
+        if ($this->getIsPickupLocationActive->execute($source) && trim($value) === '') {
             $errors[] = __('"%field" can not be empty for Pickup Location.', ['field' => SourceInterface::PHONE]);
         }
 
