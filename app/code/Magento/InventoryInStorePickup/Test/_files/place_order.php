fd085a7052205ac2a267f98d1aa38cfd1bcdfabd
Stanislav Idolov <sidolov@magento.com>
2019-11-15 13:52:58 -0600

    magento/inventory#2606: Restore integration tests fixtures

---
+++ app/code/Magento/InventoryInStorePickup/Test/_files/place_order.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Quote\Api\CartManagementInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/** @var SearchCriteriaBuilder $searchCriteriaBuilder */
+$searchCriteriaBuilder = Bootstrap::getObjectManager()->get(SearchCriteriaBuilder::class);
+/** @var CartRepositoryInterface $cartRepository */
+$cartRepository = Bootstrap::getObjectManager()->get(CartRepositoryInterface::class);
+/** @var CartManagementInterface $cartManagement */
+$cartManagement = Bootstrap::getObjectManager()->get(CartManagementInterface::class);
+
+$searchCriteria = $searchCriteriaBuilder
+    ->addFilter('reserved_order_id', 'in_store_pickup_test_order')
+    ->create();
+$cart = current($cartRepository->getList($searchCriteria)->getItems());
+
+$cartManagement->placeOrder($cart->getId());
