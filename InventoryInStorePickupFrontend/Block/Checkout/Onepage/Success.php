f95ae9802f0c0082869c43f17824bb682607987d
nmalevanec <mikola.malevanec@transoftgroup.com>
2019-11-28 18:01:01 +0200

    MSI-2685: Order success message for Store pickup order should be distinct from normal message.

---
+++ InventoryInStorePickupFrontend/Block/Checkout/Onepage/Success.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickupFrontend\Block\Checkout\Onepage;
+
+use Magento\Checkout\Block\Onepage\Success as SuccessBlock;
+use Magento\Checkout\Model\Session;
+use Magento\Framework\App\Http\Context as HttpContext;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\InventoryInStorePickupSalesApi\Model\IsStorePickupOrderInterface;
+use Magento\Sales\Model\Order\Config;
+
+/**
+ * Store pickup checkout success block.
+ *
+ * @api
+ */
+class Success extends SuccessBlock
+{
+    /**
+     * @var IsStorePickupOrderInterface
+     */
+    private $isStorePickupOrder;
+
+    /**
+     * @param Context $context
+     * @param Session $checkoutSession
+     * @param Config $orderConfig
+     * @param HttpContext $httpContext
+     * @param IsStorePickupOrderInterface $isStorePickupOrder
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Session $checkoutSession,
+        Config $orderConfig,
+        HttpContext $httpContext,
+        IsStorePickupOrderInterface $isStorePickupOrder,
+        array $data = []
+    ) {
+        parent::__construct($context, $checkoutSession, $orderConfig, $httpContext, $data);
+        $this->isStorePickupOrder = $isStorePickupOrder;
+    }
+
+    /**
+     * Get is order has pick in store delivery method.
+     *
+     * @return bool
+     */
+    public function isOrderStorePickup(): bool
+    {
+        $order = $this->_checkoutSession->getLastRealOrder();
+        return $this->isStorePickupOrder->execute((int)$order->getId());
+    }
+}
