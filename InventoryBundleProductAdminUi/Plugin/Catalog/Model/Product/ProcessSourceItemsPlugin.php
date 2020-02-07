7779a32f9d1a13a618cc543d5c1b1de2eaa2cf45
nmalevanec <mikola.malevanec@transoftgroup.com>
2020-02-07 15:34:03 +0200

    MSI-1487: Bundle product support.

--- InventoryBundleProductAdminUi/Plugin/Catalog/Model/Product/ProcessSourceItemsPlugin.php
+++ InventoryBundleProductAdminUi/Plugin/Catalog/Model/Product/ProcessSourceItemsPlugin.php
@@ -10,8 +10,10 @@ namespace Magento\InventoryBundleProductAdminUi\Plugin\Catalog\Model\Product;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Type;
 use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\InventoryApi\Api\Data\SourceItemInterface;
 use Magento\InventoryApi\Api\GetSourceItemsBySkuInterface;
+use Magento\InventoryCatalogApi\Model\GetSkusByProductIdsInterface;
 use Magento\InventoryCatalogApi\Model\SourceItemsProcessorInterface;
 use Psr\Log\LoggerInterface;
 
@@ -31,6 +33,11 @@ class ProcessSourceItemsPlugin
     private $sourceItemsProcessor;
 
     /**
+     * @var GetSkusByProductIdsInterface
+     */
+    private $getSkusByProductIds;
+
+    /**
      * @var LoggerInterface
      */
     private $logger;
@@ -38,16 +45,19 @@ class ProcessSourceItemsPlugin
     /**
      * @param GetSourceItemsBySkuInterface $getSourceItemsBySku
      * @param SourceItemsProcessorInterface $processSourceItemsForSku
+     * @param GetSkusByProductIdsInterface $getSkusByProductIds
      * @param LoggerInterface $logger
      */
     public function __construct(
         GetSourceItemsBySkuInterface $getSourceItemsBySku,
         SourceItemsProcessorInterface $processSourceItemsForSku,
+        GetSkusByProductIdsInterface $getSkusByProductIds,
         LoggerInterface $logger
     ) {
         $this->sourceItemsProcessor = $processSourceItemsForSku;
-        $this->logger = $logger;
         $this->getSourceItemsBySku = $getSourceItemsBySku;
+        $this->getSkusByProductIds = $getSkusByProductIds;
+        $this->logger = $logger;
     }
 
     /**
@@ -59,7 +69,7 @@ class ProcessSourceItemsPlugin
      */
     public function afterAfterSave(Product $subject, Product $result): Product
     {
-        if (!$subject->getTypeId() === Type::TYPE_BUNDLE) {
+        if ($subject->getTypeId() !== Type::TYPE_BUNDLE) {
             return $result;
         }
 
@@ -67,12 +77,14 @@ class ProcessSourceItemsPlugin
         foreach ($options as $option) {
             foreach ($option as $selection) {
                 try {
-                    $sku = (string)$selection['sku'];
-                    $this->sourceItemsProcessor->execute(
-                        $sku,
-                        $this->getSourceItemsData($sku)
-                    );
-                } catch (InputException $e) {
+                    $sku = $this->getSelectionSku($selection);
+                    if ($sku) {
+                        $this->sourceItemsProcessor->execute(
+                            $sku,
+                            $this->getSourceItemsData($sku)
+                        );
+                    }
+                } catch (InputException|NoSuchEntityException $e) {
                     $this->logger->error($e->getLogMessage());
                     continue;
                 }
@@ -100,5 +112,23 @@ class ProcessSourceItemsPlugin
         }
 
         return $data;
+    }
+
+    /**
+     * Retrieve bundle selection sku.
+     *
+     * @param array $selection
+     * @return string
+     * @throws NoSuchEntityException
+     */
+    private function getSelectionSku(array $selection): string
+    {
+        $sku = $selection['sku'] ?? '';
+
+        if (!$sku && isset($selection['product_id'])) {
+            $sku =$this->getSkusByProductIds->execute([$selection['product_id']])[$selection['product_id']];
+        }
+
+        return $sku;
     }
 }
