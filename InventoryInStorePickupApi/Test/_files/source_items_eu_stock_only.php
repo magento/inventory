ce6ac758efc3dbd2b343dd97d26a8c7441e2b269
oleksandrkravchuk <oleksandr.kravchuk@vaimo.com>
2020-03-09 20:39:04 +0100

    MSI-2825-Hide-store-pickup-on-the-Checkout-if-one-of-the-products-in-cart-is-not-available-for-pickup. Fix tests.

--- InventoryInStorePickupApi/Test/_files/source_items_eu_stock_only.php
+++ InventoryInStorePickupApi/Test/_files/source_items_eu_stock_only.php
@@ -18,17 +18,6 @@ $sourceItemFactory = Bootstrap::getObjectManager()->get(SourceItemInterfaceFacto
 /** @var  SourceItemsSaveInterface $sourceItemsSave */
 $sourceItemsSave = Bootstrap::getObjectManager()->get(SourceItemsSaveInterface::class);
 
-/**
- * SKU-1 - EU-source-1(id:10) - 5.5qty
- * SKU-1 - EU-source-2(id:20) - 3qty
- * SKU-1 - EU-source-3(id:30) - 10qty
- * SKU-1 - EU-source-4(id:40) - 10qty (disabled source)
- *
- * SKU-2 - US-source-1(id:30) - 5qty
- * SKU-2 - EU-source-2(id:20) - 3qty
- *
- * SKU-3 - EU-source-2(id:20) - 6qty
- */
 $sourcesItemsData = [
     [
         SourceItemInterface::SOURCE_CODE => 'eu-1',
@@ -39,26 +28,14 @@ $sourcesItemsData = [
     [
         SourceItemInterface::SOURCE_CODE => 'eu-2',
         SourceItemInterface::SKU => 'SKU-1',
-        SourceItemInterface::QUANTITY => 3.5,
-        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
+        SourceItemInterface::QUANTITY => 0,
+        SourceItemInterface::STATUS => SourceItemInterface::STATUS_OUT_OF_STOCK,
     ],
     [
         SourceItemInterface::SOURCE_CODE => 'eu-3',
         SourceItemInterface::SKU => 'SKU-1',
-        SourceItemInterface::QUANTITY => 10,
-        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
-    ],
-    [
-        SourceItemInterface::SOURCE_CODE => 'eu-disabled',
-        SourceItemInterface::SKU => 'SKU-1',
-        SourceItemInterface::QUANTITY => 10,
-        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
-    ],
-    [
-        SourceItemInterface::SOURCE_CODE => 'us-1',
-        SourceItemInterface::SKU => 'SKU-2',
-        SourceItemInterface::QUANTITY => 5,
-        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
+        SourceItemInterface::QUANTITY => 0,
+        SourceItemInterface::STATUS => SourceItemInterface::STATUS_OUT_OF_STOCK,
     ],
     [
         SourceItemInterface::SOURCE_CODE => 'eu-1',
@@ -79,28 +56,22 @@ $sourcesItemsData = [
         SourceItemInterface::STATUS => SourceItemInterface::STATUS_OUT_OF_STOCK,
     ],
     [
-        SourceItemInterface::SOURCE_CODE => 'eu-disabled',
-        SourceItemInterface::SKU => 'SKU-2',
+        SourceItemInterface::SOURCE_CODE => 'eu-1',
+        SourceItemInterface::SKU => 'SKU-3',
         SourceItemInterface::QUANTITY => 0,
         SourceItemInterface::STATUS => SourceItemInterface::STATUS_OUT_OF_STOCK,
     ],
     [
+        SourceItemInterface::SOURCE_CODE => 'eu-3',
+        SourceItemInterface::SKU => 'SKU-3',
+        SourceItemInterface::QUANTITY => 10,
+        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
+    ],
+    [
         SourceItemInterface::SOURCE_CODE => 'eu-2',
         SourceItemInterface::SKU => 'SKU-3',
-        SourceItemInterface::QUANTITY => 6,
-        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
-    ],
-    [
-        SourceItemInterface::SOURCE_CODE => 'eu-2',
-        SourceItemInterface::SKU => 'SKU-4',
-        SourceItemInterface::QUANTITY => 6,
-        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
-    ],
-    [
-        SourceItemInterface::SOURCE_CODE => 'eu-1',
-        SourceItemInterface::SKU => 'SKU-6',
-        SourceItemInterface::QUANTITY => 666,
-        SourceItemInterface::STATUS => SourceItemInterface::STATUS_IN_STOCK,
+        SourceItemInterface::QUANTITY => 0,
+        SourceItemInterface::STATUS => SourceItemInterface::STATUS_OUT_OF_STOCK,
     ],
 ];
 
