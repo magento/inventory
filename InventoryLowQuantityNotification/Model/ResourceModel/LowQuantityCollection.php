181b2766d93d057b27f317016a688c78c437347a
nmalevanec <mikola.malevanec@transoftgroup.com>
2020-03-26 15:35:06 +0200

    MSI-2899: Low stock report in scope of website works incorrectly.

--- InventoryLowQuantityNotification/Model/ResourceModel/LowQuantityCollection.php
+++ InventoryLowQuantityNotification/Model/ResourceModel/LowQuantityCollection.php
@@ -111,6 +111,8 @@ class LowQuantityCollection extends AbstractCollection
     }
 
     /**
+     * Set store id to filter.
+     *
      * @param int $storeId
      * @return void
      */
@@ -132,6 +134,7 @@ class LowQuantityCollection extends AbstractCollection
             $this->addNotifyStockQtyFilter();
             $this->addEnabledSourceFilter();
             $this->addSourceItemInStockFilter();
+            $this->addSourceItemStoreFilter();
         }
         return parent::_renderFilters();
     }
@@ -148,7 +151,7 @@ class LowQuantityCollection extends AbstractCollection
     }
 
     /**
-     * joinCatalogProduct depends on dynamic condition 'filterStoreId'
+     * JoinCatalogProduct depends on dynamic condition 'filterStoreId'
      *
      * @return void
      */
@@ -170,7 +173,7 @@ class LowQuantityCollection extends AbstractCollection
         $this->getSelect()->joinInner(
             ['product_entity_varchar' => $productEavVarcharTable],
             'product_entity_varchar.' . $linkField . ' = product_entity.' . $linkField . ' ' .
-            'AND product_entity_varchar.store_id = ' . Store::DEFAULT_STORE_ID. ' ' .
+            'AND product_entity_varchar.store_id = ' . Store::DEFAULT_STORE_ID . ' ' .
             'AND product_entity_varchar.attribute_id = ' . (int)$nameAttribute->getAttributeId(),
             []
         );
@@ -185,7 +188,7 @@ class LowQuantityCollection extends AbstractCollection
                     'product_name' => $this->getConnection()->getIfNullSql(
                         'product_entity_varchar_store.value',
                         'product_entity_varchar.value'
-                    )
+                    ),
                 ]
             );
         } else {
@@ -194,9 +197,11 @@ class LowQuantityCollection extends AbstractCollection
     }
 
     /**
+     * Add inventory configuration information to collection.
+     *
      * @return void
      */
-    private function joinInventoryConfiguration()
+    private function joinInventoryConfiguration(): void
     {
         $sourceItemConfigurationTable = $this->getTable('inventory_low_stock_notification_configuration');
 
@@ -214,9 +219,11 @@ class LowQuantityCollection extends AbstractCollection
     }
 
     /**
+     * Filter allowed product types.
+     *
      * @return void
      */
-    private function addProductTypeFilter()
+    private function addProductTypeFilter(): void
     {
         $this->addFieldToFilter(
             'product_entity.type_id',
@@ -225,9 +232,11 @@ class LowQuantityCollection extends AbstractCollection
     }
 
     /**
+     * Add notify configuration information to collection.
+     *
      * @return void
      */
-    private function addNotifyStockQtyFilter()
+    private function addNotifyStockQtyFilter(): void
     {
         $notifyStockExpression = $this->getConnection()->getIfNullSql(
             'notification_configuration.' . SourceItemConfigurationInterface::INVENTORY_NOTIFY_QTY,
@@ -241,9 +250,11 @@ class LowQuantityCollection extends AbstractCollection
     }
 
     /**
+     * Filter disabled sources.
+     *
      * @return void
      */
-    private function addEnabledSourceFilter()
+    private function addEnabledSourceFilter(): void
     {
         $this->getSelect()->joinInner(
             ['inventory_source' => $this->getTable(Source::TABLE_NAME_SOURCE)],
@@ -258,10 +269,46 @@ class LowQuantityCollection extends AbstractCollection
     }
 
     /**
+     * Filter out of stock source items.
+     *
      * @return void
      */
-    private function addSourceItemInStockFilter()
+    private function addSourceItemInStockFilter(): void
     {
         $this->addFieldToFilter('main_table.status', SourceItemInterface::STATUS_IN_STOCK);
+    }
+
+    /**
+     * Filter source items by store if provided.
+     *
+     * @return void
+     */
+    private function addSourceItemStoreFilter(): void
+    {
+        if ($this->filterStoreId === null) {
+            return;
+        }
+
+        $this->getSelect()->joinInner(
+            ['source_stock_link' => $this->getTable('inventory_source_stock_link')],
+            'source_stock_link.source_code = inventory_source.source_code',
+            []
+        )->joinInner(
+            ['stock' => $this->getTable('inventory_stock')],
+            'stock.stock_id = source_stock_link.stock_id',
+            []
+        )->joinInner(
+            ['sales_channel' => $this->getTable('inventory_stock_sales_channel')],
+            'sales_channel.stock_id = stock.stock_id',
+            []
+        )->joinInner(
+            ['website' => $this->getTable('store_website')],
+            'website.code = sales_channel.code and sales_channel.type = "website"',
+            []
+        )->joinInner(
+            ['store' => $this->getTable('store')],
+            'store.website_id = website.website_id and store.store_id = ' . $this->filterStoreId,
+            []
+        );
     }
 }
