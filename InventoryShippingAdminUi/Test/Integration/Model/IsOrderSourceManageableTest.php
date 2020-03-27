086fc48d8c6bcd65c23423b72f039b3b8c7f7ecc
nmalevanec <mikola.malevanec@transoftgroup.com>
2020-03-27 11:21:32 +0200

    Fix exception when a product is not assigned to all stocks. Fix test.

--- InventoryShippingAdminUi/Test/Integration/Model/IsOrderSourceManageableTest.php
+++ InventoryShippingAdminUi/Test/Integration/Model/IsOrderSourceManageableTest.php
@@ -7,6 +7,9 @@ declare(strict_types=1);
 
 namespace Magento\InventoryShippingAdminUi\Test\Integration\Model;
 
+use Magento\CatalogInventory\Api\StockItemRepositoryInterface;
+use Magento\InventoryCatalog\Model\DeleteSourceItemsBySkus;
+use Magento\InventoryConfiguration\Model\GetLegacyStockItem;
 use Magento\InventoryShippingAdminUi\Model\IsOrderSourceManageable;
 use Magento\Sales\Model\OrderFactory;
 use Magento\TestFramework\Helper\Bootstrap;
@@ -30,21 +33,46 @@ class IsOrderSourceManageableTest extends TestCase
     protected function setUp()
     {
         $this->isOrderSourceManageable = Bootstrap::getObjectManager()->get(IsOrderSourceManageable::class);
+        $this->disableManageStock();
+        $this->cleanUpSourceItems();
     }
 
     /**
      * Verify order with not assigned to all stocks items won't throw exception.
      *
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryIndexer/Test/_files/reindex_inventory.php
      * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stocks.php
      * @magentoDataFixture Magento/Sales/_files/order.php
-     *
-     * @magentoDbIsolation disabled
      */
     public function testOrderWithProductsNotAssignedToStocks(): void
     {
         $order = Bootstrap::getObjectManager()->get(OrderFactory::class)->create()->loadByIncrementId('100000001');
         $result = $this->isOrderSourceManageable->execute($order);
         self::assertFalse($result);
+    }
+
+    /**
+     * Clean up source items for test product as they may be left by other tests run.
+     *
+     * @return void
+     */
+    private function cleanUpSourceItems(): void
+    {
+        $deleteSourceItems = Bootstrap::getObjectManager()->get(DeleteSourceItemsBySkus::class);
+        $deleteSourceItems->execute(['simple']);
+    }
+
+    /**
+     * Disable manage stock for default stock item.
+     *
+     * @return void
+     */
+    private function disableManageStock(): void
+    {
+        $getLegacyStockItem = Bootstrap::getObjectManager()->get(GetLegacyStockItem::class);
+        $stockItemRepository = Bootstrap::getObjectManager()->get(StockItemRepositoryInterface::class);
+        $stockItem = $getLegacyStockItem->execute('simple');
+        $stockItem->setManageStock(false);
+        $stockItem->setUseConfigManageStock(false);
+        $stockItemRepository->save($stockItem);
     }
 }
