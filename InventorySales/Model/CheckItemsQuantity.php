68922732d3a5708be1fef271005af99a61b71158
Stanislav Idolov <sidolov@adobe.com>
2020-04-01 11:05:42 -0500

    Code style fix

--- InventorySales/Model/CheckItemsQuantity.php
+++ InventorySales/Model/CheckItemsQuantity.php
@@ -12,6 +12,9 @@ use Magento\InventorySalesApi\Api\IsProductSalableForRequestedQtyInterface;
 use Magento\InventorySalesApi\Api\Data\ProductSalableResultInterface;
 use Magento\InventorySalesApi\Api\Data\ProductSalabilityErrorInterface;
 
+/**
+ * Verify items are salable for requested quantity.
+ */
 class CheckItemsQuantity
 {
     /**
