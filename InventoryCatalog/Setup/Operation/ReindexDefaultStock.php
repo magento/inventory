16829e3256065f5135efc1c0c7e77efc14b56013
nmalevanec <mikola.malevanec@transoftgroup.com>
2020-04-08 17:44:43 +0300

    Remove MySQL view and remove check for default stock.

--- InventoryCatalog/Setup/Operation/ReindexDefaultStock.php
+++ InventoryCatalog/Setup/Operation/ReindexDefaultStock.php
@@ -7,8 +7,14 @@ declare(strict_types=1);
 
 namespace Magento\InventoryCatalog\Setup\Operation;
 
-use Magento\InventoryIndexer\Indexer\Stock\StockIndexer;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Exception\StateException;
 use Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface;
+use Magento\InventoryIndexer\Indexer\InventoryIndexer;
+use Magento\InventoryIndexer\Indexer\Stock\StockIndexer;
+use Magento\InventoryMultiDimensionalIndexerApi\Model\Alias;
+use Magento\InventoryMultiDimensionalIndexerApi\Model\IndexNameBuilder;
+use Magento\InventoryMultiDimensionalIndexerApi\Model\IndexStructureInterface;
 
 /**
  * CReindex default stock during installation
@@ -26,24 +32,51 @@ class ReindexDefaultStock
     private $stockIndexer;
 
     /**
+     * @var IndexNameBuilder
+     */
+    private $indexNameBuilder;
+
+    /**
+     * @var IndexStructureInterface
+     */
+    private $indexStructure;
+
+    /**
      * @param DefaultStockProviderInterface $defaultStockProvider
      * @param StockIndexer $stockIndexer
+     * @param IndexNameBuilder $indexNameBuilder
+     * @param IndexStructureInterface $indexStructure
      */
     public function __construct(
         DefaultStockProviderInterface $defaultStockProvider,
-        StockIndexer $stockIndexer
+        StockIndexer $stockIndexer,
+        IndexNameBuilder $indexNameBuilder,
+        IndexStructureInterface $indexStructure
     ) {
         $this->defaultStockProvider = $defaultStockProvider;
         $this->stockIndexer = $stockIndexer;
+        $this->indexNameBuilder = $indexNameBuilder;
+        $this->indexStructure = $indexStructure;
     }
 
     /**
-     * Create default stock
+     * Create default stock.
      *
      * @return void
+     * @throws StateException
      */
     public function execute()
     {
-//        $this->stockIndexer->executeRow($this->defaultStockProvider->getId());
+        $stockId = $this->defaultStockProvider->getId();
+        $mainIndexName = $this->indexNameBuilder
+            ->setIndexId(InventoryIndexer::INDEXER_ID)
+            ->addDimension('stock_', (string)$stockId)
+            ->setAlias(Alias::ALIAS_MAIN)
+            ->build();
+
+        if (!$this->indexStructure->isExist($mainIndexName, ResourceConnection::DEFAULT_CONNECTION)) {
+            $this->indexStructure->create($mainIndexName, ResourceConnection::DEFAULT_CONNECTION);
+        }
+        $this->stockIndexer->executeRow($this->defaultStockProvider->getId());
     }
 }
