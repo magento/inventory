3ce8c9513759a75b6278a5740ed91a8de2199e19
Michail Slabko <mslabko@magento.com>
2020-04-21 18:45:50 -0500

    MC-33114: [B2B] Quickorder Add To Cart is significant slow

--- InventoryAdvancedCheckout/Plugin/Model/AreProductsSalablePlugin.php
+++ InventoryAdvancedCheckout/Plugin/Model/AreProductsSalablePlugin.php
@@ -5,10 +5,9 @@
  */
 declare(strict_types=1);
 
-namespace Magento\InventoryAdvancedCheckout\Plugin\Model\IsProductInStock;
+namespace Magento\InventoryAdvancedCheckout\Plugin\Model;
 
-use Magento\AdvancedCheckout\Model\IsProductInStockInterface;
-use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\AdvancedCheckout\Model\AreProductsSalableForRequestedQtyInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface;
 use Magento\InventorySalesApi\Api\AreProductsSalableInterface;
@@ -19,13 +18,8 @@ use Magento\Store\Api\WebsiteRepositoryInterface;
 /**
  * Provides multi-sourcing capabilities for Advanced Checkout Order By SKU feature.
  */
-class ProductInStockPlugin
+class AreProductsSalablePlugin
 {
-    /**
-     * @var ProductRepositoryInterface
-     */
-    private $productRepository;
-
     /**
      * @var AreProductsSalableInterface
      */
@@ -47,20 +41,17 @@ class ProductInStockPlugin
     private $defaultStockProvider;
 
     /**
-     * @param ProductRepositoryInterface $productRepository
      * @param AreProductsSalableInterface $areProductsSalable
      * @param StockResolverInterface $stockResolver
      * @param WebsiteRepositoryInterface $websiteRepository
      * @param DefaultStockProviderInterface $defaultStockProvider
      */
     public function __construct(
-        ProductRepositoryInterface $productRepository,
         AreProductsSalableInterface $areProductsSalable,
         StockResolverInterface $stockResolver,
         WebsiteRepositoryInterface $websiteRepository,
         DefaultStockProviderInterface $defaultStockProvider
     ) {
-        $this->productRepository = $productRepository;
         $this->areProductsSalable = $areProductsSalable;
         $this->stockResolver = $stockResolver;
         $this->websiteRepository = $websiteRepository;
@@ -70,28 +61,31 @@ class ProductInStockPlugin
     /**
      * Get is product out of stock for given Product id in a given Website id in MSI context.
      *
-     * @param IsProductInStockInterface $subject
+     * @param AreProductsSalableForRequestedQtyInterface $subject
      * @param callable $proceed
-     * @param int $productId
+     * @param \Magento\AdvancedCheckout\Model\Data\ProductQuantity[] $productQuantities
      * @param int $websiteId
      * @return bool
      * @throws NoSuchEntityException
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function aroundExecute(
-        IsProductInStockInterface $subject,
+        AreProductsSalableForRequestedQtyInterface $subject,
         callable $proceed,
-        int $productId,
+        array $productQuantities,
         int $websiteId
     ): bool {
-        $product = $this->productRepository->getById($productId);
         $website = $this->websiteRepository->getById($websiteId);
         $stock = $this->stockResolver->execute(SalesChannelInterface::TYPE_WEBSITE, $website->getCode());
         if ($this->defaultStockProvider->getId() === $stock->getStockId()) {
-            return $proceed($productId, $websiteId);
+            return $proceed($productQuantities, $websiteId);
         }
 
-        $result = $this->areProductsSalable->execute([$product->getSku()], $stock->getStockId());
+        $skus = [];
+        foreach ($productQuantities as $productQuantity) {
+            $skus[] = $productQuantity->getSku();
+        }
+        $result = $this->areProductsSalable->execute($skus, $stock->getStockId());
         $result = current($result);
 
         return $result->isSalable();
