4bc0e480add70b9874073a09b346c3ec1b89f857
Lyzun Oleksandr <alex.nuzil@gmail.com>
2020-05-06 16:46:37 +0200

    Add queues for reservation placed

---
+++ InventoryIndexer/Model/ReservationDummy.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryIndexer\Model;
+
+/**
+ * ----
+ */
+class ReservationDummy
+{
+    /**
+     * @param string[] $array
+     * @return void
+     */
+    public function execute($array)
+    {
+        return true;
+    }
+}
