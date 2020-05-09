bd26ecc7a1010c005a0b7b1f261ce7d26ca6df75
Maksym Novik <novik.kor@gmail.com>
2020-05-09 16:28:39 +0300

    [Async Salable Status] Create infrastructure for async status update #3012. Updated queue message format.

---
+++ InventoryIndexer/Model/Queue/RecalculateIndexSalabilityStatus.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryIndexer\Model\Queue;
+
+/**
+ * Recalculates index items salability status.
+ */
+class RecalculateIndexSalabilityStatus
+{
+    /**
+     * @param ReservationData $reservationData
+     *
+     * @return void
+     */
+    public function execute(ReservationData $reservationData): void
+    {
+        //TODO
+    }
+}
