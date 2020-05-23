a9772174284b9615efbd3da30ed1fe834da8585d
Stanislav Idolov <sidolov@magento.com>
2020-05-23 17:53:28 +0300

    Load product salability data on-demand #3201. Fixed is salable logic.

--- InventoryCatalog/Plugin/Catalog/Model/Product/IsSalablePlugin.php
+++ InventoryCatalog/Plugin/Catalog/Model/Product/IsSalablePlugin.php
@@ -3,15 +3,15 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 declare(strict_types=1);
 
 namespace Magento\InventoryCatalog\Plugin\Catalog\Model\Product;
 
 use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Type\Simple;
 use Magento\InventoryCatalog\Model\IsProductSalable;
 
-class IsAvailable
+class IsSalablePlugin
 {
     /**
      * @var IsProductSalable
@@ -28,11 +28,13 @@ class IsAvailable
     }
 
     /**
-     * @param Product $product
+     * Fetches is salable status from multi-stock.
      *
+     * @param Product $product
      * @return bool
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function aroundIsAvailable(Product $product): bool
+    public function aroundIsSalable(Product $product): bool
     {
         return $this->isProductSalable->execute($product);
     }
