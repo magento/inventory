037dd956690402731268259350e78c40d64f465a
oleksandrkravchuk <oleksandr.kravchuk@vaimo.com>
2020-05-28 17:36:39 +0200

    Add logging.

--- InventoryIndexer/Model/IsProductSalable.php
+++ InventoryIndexer/Model/IsProductSalable.php
@@ -10,6 +10,7 @@ namespace Magento\InventoryIndexer\Model;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\InventorySalesApi\Api\IsProductSalableInterface;
 use Magento\InventorySalesApi\Model\GetStockItemDataInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Lightweight implementation for Storefront application.
@@ -20,13 +21,21 @@ class IsProductSalable implements IsProductSalableInterface
      * @var GetStockItemDataInterface
      */
     private $getStockItemData;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * @param GetStockItemDataInterface $getStockItemData
+     * @param LoggerInterface $logger
      */
-    public function __construct(GetStockItemDataInterface $getStockItemData)
-    {
+    public function __construct(
+        GetStockItemDataInterface $getStockItemData,
+        LoggerInterface $logger
+    ) {
         $this->getStockItemData = $getStockItemData;
+        $this->logger = $logger;
     }
 
     /**
@@ -38,6 +47,13 @@ class IsProductSalable implements IsProductSalableInterface
             $stockItem = $this->getStockItemData->execute($sku, $stockId);
             $isSalable = (bool)($stockItem[GetStockItemDataInterface::IS_SALABLE] ?? false);
         } catch (LocalizedException $exception) {
+            $this->logger->warning(sprintf(
+                    'Unable to fetch stock #%s data for SKU %s. Reason: %s',
+                    $stockId,
+                    $sku,
+                    $exception->getMessage()
+                )
+            );
             $isSalable = false;
         }
 
