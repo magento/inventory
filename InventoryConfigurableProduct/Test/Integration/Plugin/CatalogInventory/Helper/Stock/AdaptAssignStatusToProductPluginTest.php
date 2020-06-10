e4b62f72a51b579fed210fabfc8fdb3a9b4059fa
Oleksandr Dubovyk <odubovyk@magento.com>
2020-06-09 19:44:22 -0500

    MC-34351: [On-Premise] Configurable product out of stock cart issue
    - fixed static

--- InventoryConfigurableProduct/Test/Integration/Plugin/CatalogInventory/Helper/Stock/AdaptAssignStatusToProductPluginTest.php
+++ InventoryConfigurableProduct/Test/Integration/Plugin/CatalogInventory/Helper/Stock/AdaptAssignStatusToProductPluginTest.php
@@ -23,7 +23,7 @@ class AdaptAssignStatusToProductPluginTest extends TestCase
     /**
      * @inheritdoc
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
