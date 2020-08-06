46ddcee5fcb134a752724ad50071e968398a6422
Vasya Tsviklinskyi <tsviklinskyi@gmail.com>
2020-08-06 10:13:20 +0300

    MC-35574: show source stock instead of default stock

--- InventoryVisualMerchandiser/Test/Integration/Model/Category/ProductsTest.php
+++ InventoryVisualMerchandiser/Test/Integration/Model/Category/ProductsTest.php
@@ -18,14 +18,14 @@ class ProductsTest extends TestCase
     /**
      * Test that products in category have correct quantity by source
      *
-     * @magentoDataFixture ../../../../app/code/Magento/InventorySalesApi/Test/_files/websites_with_stores.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryVisualMerchandiser/Test/_files/product_with_category.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/sources.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stocks.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stock_source_links.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryVisualMerchandiser/Test/_files/source_items_products.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventorySalesApi/Test/_files/stock_website_sales_channels.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryIndexer/Test/_files/reindex_inventory.php
+     * @magentoDataFixture Magento_InventorySalesApi::Test/_files/websites_with_stores.php
+     * @magentoDataFixture Magento_InventoryVisualMerchandiser::Test/_files/product_with_category.php
+     * @magentoDataFixture Magento_InventoryApi::Test/_files/sources.php
+     * @magentoDataFixture Magento_InventoryApi::Test/_files/stocks.php
+     * @magentoDataFixture Magento_InventoryApi::Test/_files/stock_source_links.php
+     * @magentoDataFixture Magento_InventoryVisualMerchandiser::Test/_files/source_items_products.php
+     * @magentoDataFixture Magento_InventorySalesApi::Test/_files/stock_website_sales_channels.php
+     * @magentoDataFixture Magento_InventoryIndexer::Test/_files/reindex_inventory.php
      * @magentoDbIsolation disabled
      */
     public function testProductsInCategory(): void
