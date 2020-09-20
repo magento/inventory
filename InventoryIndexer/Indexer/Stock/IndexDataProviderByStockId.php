c606e2f89e3a06e79793d3cd042e432460efc098
Oleksandr Iegorov <oiegorov@adobe.com>
2020-09-20 15:41:13 -0500

    MC-37674: Missing products from categories, indexing related

--- InventoryIndexer/Indexer/Stock/IndexDataProviderByStockId.php
+++ InventoryIndexer/Indexer/Stock/IndexDataProviderByStockId.php
@@ -10,6 +10,7 @@ namespace Magento\InventoryIndexer\Indexer\Stock;
 use ArrayIterator;
 use Magento\Framework\App\ResourceConnection;
 use Magento\InventoryIndexer\Indexer\SelectBuilder;
+use Magento\InventoryConfigurableProductIndexer\Indexer\SelectBuilder as LinkedSelectBuilder;
 
 /**
  * Returns all data for the index
@@ -27,26 +28,39 @@ class IndexDataProviderByStockId
     private $selectBuilder;
 
     /**
+     * @var LinkedSelectBuilder
+     */
+    private $linkedSelectBuilder;
+
+    /**
      * @param ResourceConnection $resourceConnection
      * @param SelectBuilder $selectBuilder
+     * @param LinkedSelectBuilder $linkedSelectBuilder
      */
     public function __construct(
         ResourceConnection $resourceConnection,
-        SelectBuilder $selectBuilder
+        SelectBuilder $selectBuilder,
+        LinkedSelectBuilder $linkedSelectBuilder
     ) {
         $this->resourceConnection = $resourceConnection;
         $this->selectBuilder = $selectBuilder;
+        $this->linkedSelectBuilder = $linkedSelectBuilder;
     }
 
     /**
      * @param int $stockId
+     * @throws \Exception
      * @return ArrayIterator
      */
     public function execute(int $stockId): ArrayIterator
     {
+        $result = [];
         $select = $this->selectBuilder->execute($stockId);
 
         $connection = $this->resourceConnection->getConnection();
-        return new ArrayIterator($connection->fetchAll($select));
+        $result = array_merge($result, $connection->fetchAll($select));
+        $linkedSelect = $this->linkedSelectBuilder->execute($stockId);
+        $result = array_merge($result, $connection->fetchAll($linkedSelect));
+        return new ArrayIterator($result);
     }
 }
