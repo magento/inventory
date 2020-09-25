0932a03a2354e3d0ae3f31a7d2468938112ccb90
Buba Suma <soumah@adobe.com>
2020-09-25 15:29:43 -0500

    MC-34351: [On-Premise] Configurable product out of stock cart issue
    - Refactor code

--- InventoryConfigurableProduct/Plugin/CatalogInventory/Helper/Stock/AdaptAssignStatusToProductPlugin.php
+++ InventoryConfigurableProduct/Plugin/CatalogInventory/Helper/Stock/AdaptAssignStatusToProductPlugin.php
@@ -89,14 +89,11 @@ class AdaptAssignStatusToProductPlugin
             $stockId = $stock->getStockId();
             try {
                 $stockItemData = $this->getStockItemData->execute($product->getSku(), $stockId);
-            } catch (NoSuchEntityException $exception) {
-                $stockItemData = null;
-            }
-            if (null !== $stockItemData) {
-                $salableConfigurable = (bool)$stockItemData[GetStockItemDataInterface::IS_SALABLE];
-                if (!$salableConfigurable) {
+                if (!((bool) $stockItemData[GetStockItemDataInterface::IS_SALABLE])) {
                     return [$product, $status];
                 }
+            } catch (NoSuchEntityException $exception) {
+                $stockItemData = null;
             }
             $options = $this->configurable->getConfigurableOptions($product);
             $status = 0;
