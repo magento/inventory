18f70f6e23507c7c9a645cd05d950e6a7097fb91
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2020-10-19 13:02:39 +0300

    MC-17694: Inventory Threshold doesn't display for configurable products simple product

---
+++ InventorySwatchesFrontendUi/view/frontend/requirejs-config.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    config: {
+        mixins: {
+            'Magento_Swatches/js/swatch-renderer': {
+                'Magento_InventorySwatchesFrontendUi/js/swatch-renderer': true
+            }
+        }
+    }
+};
