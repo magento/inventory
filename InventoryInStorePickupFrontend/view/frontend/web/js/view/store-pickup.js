df584a57ceac1612f113e7fc9411d8caffe306b2
Nikita Shcherbatykh <nikita.shcherbatykh@transoftgroup.com>
2021-01-18 15:35:21 +0200

    MC-40303: Store address overrides customer information when in-store delivery is enabled

--- InventoryInStorePickupFrontend/view/frontend/web/js/view/store-pickup.js
+++ InventoryInStorePickupFrontend/view/frontend/web/js/view/store-pickup.js
@@ -162,6 +162,7 @@ define([
             checkoutData.setSelectedShippingAddress(
                 quote.shippingAddress().getKey()
             );
+            checkoutData.setSelectedShippingRate(this.rate['carrier_code'] + '_' + this.rate['method_code']);
         },
 
         /**
@@ -265,6 +266,7 @@ define([
                         }
                     });
             }
+            this.convertAddressType(quote.shippingAddress());
         },
 
         /**
