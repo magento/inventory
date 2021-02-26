9ec59b0a311bed35064f208235a63f51feab8591
Medvediev <v.medvediev@atwix.com>
2021-02-26 14:03:22 +0200

    Using \Magento\Framework\Data\OptionSourceInterface instead of Magento\Framework\Option\ArrayInterface

--- InventoryDistanceBasedSourceSelectionAdminUi/Model/Config/Source/GoogleDistanceProvider/Value.php
+++ InventoryDistanceBasedSourceSelectionAdminUi/Model/Config/Source/GoogleDistanceProvider/Value.php
@@ -7,9 +7,9 @@ declare(strict_types=1);
 
 namespace Magento\InventoryDistanceBasedSourceSelectionAdminUi\Model\Config\Source\GoogleDistanceProvider;
 
-use Magento\Framework\Option\ArrayInterface;
+use Magento\Framework\Data\OptionSourceInterface;
 
-class Value implements ArrayInterface
+class Value implements OptionSourceInterface
 {
     private const MODE_DISTANCE = 'distance';
     private const MODE_TIME = 'time';
