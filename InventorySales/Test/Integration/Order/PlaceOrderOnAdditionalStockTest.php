e9a4841d3dc61082ac93c2ae5c3f2ccf1036f030
Yurii Sapiha <yurasapiga93@gmail.com>
2021-03-29 14:50:14 +0300

    MQE-2553: Create automated test for: "Reservations updated when SKU of Simple product on Test stock has been changed"

--- InventorySales/Test/Integration/Order/PlaceOrderOnAdditionalStockTest.php
+++ InventorySales/Test/Integration/Order/PlaceOrderOnAdditionalStockTest.php
@@ -8,16 +8,15 @@ declare(strict_types=1);
 namespace Magento\InventorySales\Test\Integration\Order;
 
 use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\App\Request\Http as HttpRequest;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Exception\StateException;
 use Magento\Framework\MessageQueue\MessageEncoder;
 use Magento\Framework\Registry;
 use Magento\InventoryCatalogAdminUi\Model\GetSourceItemsDataBySku;
-use Magento\InventoryConfigurationApi\Api\GetStockItemConfigurationInterface;
-use Magento\InventoryConfigurationApi\Api\SaveStockItemConfigurationInterface;
+use Magento\InventoryReservationsApi\Model\CleanupReservationsInterface;
 use Magento\InventorySales\Model\ResourceModel\UpdateReservationsBySkus;
 use Magento\MysqlMq\Model\Driver\Queue;
 use Magento\Quote\Api\CartManagementInterface;
@@ -27,12 +26,9 @@ use Magento\Quote\Api\Data\CartItemInterface;
 use Magento\Quote\Api\Data\CartItemInterfaceFactory;
 use Magento\Sales\Api\OrderManagementInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Store\Api\StoreRepositoryInterface;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\TestFramework\Store\ExecuteInStoreContext;
 use Magento\TestFramework\TestCase\AbstractBackendController;
-use Magento\InventoryReservationsApi\Model\CleanupReservationsInterface;
-use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\InventoryApi\Api\StockRepositoryInterface;
 
 /**
  * Class to test place order to additional stock
@@ -67,16 +63,6 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
     private $cartItemFactory;
 
     /**
-     * @var StockRepositoryInterface
-     */
-    private $stockRepository;
-
-    /**
-     * @var StoreRepositoryInterface
-     */
-    private $storeRepository;
-
-    /**
      * @var SearchCriteriaBuilder
      */
     private $searchCriteriaBuilder;
@@ -100,16 +86,6 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
      * @var StoreManagerInterface
      */
     private $storeManager;
-
-    /**
-     * @var GetStockItemConfigurationInterface
-     */
-    private $getStockItemConfiguration;
-
-    /**
-     * @var SaveStockItemConfigurationInterface
-     */
-    private $saveStockItemConfiguration;
 
     /**
      * @var UpdateReservationsBySkus
@@ -147,6 +123,11 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
     private $getSourceItemsDataBySku;
 
     /**
+     * @var ExecuteInStoreContext
+     */
+    private $executeInStoreContext;
+
+    /**
      * @inheritdoc
      */
     protected function setUp(): void
@@ -154,8 +135,6 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
         parent::setUp();
 
         $this->registry = $this->_objectManager->get(Registry::class);
-        $this->stockRepository = $this->_objectManager->get(StockRepositoryInterface::class);
-        $this->storeRepository = $this->_objectManager->get(StoreRepositoryInterface::class);
         $this->storeManager = $this->_objectManager->get(StoreManagerInterface::class);
         $this->cartManagement = $this->_objectManager->get(CartManagementInterface::class);
         $this->cartRepository = $this->_objectManager->get(CartRepositoryInterface::class);
@@ -166,13 +145,12 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
         $this->cleanupReservations = $this->_objectManager->get(CleanupReservationsInterface::class);
         $this->orderRepository = $this->_objectManager->get(OrderRepositoryInterface::class);
         $this->orderManagement = $this->_objectManager->get(OrderManagementInterface::class);
-        $this->getStockItemConfiguration = $this->_objectManager->get(GetStockItemConfigurationInterface::class);
-        $this->saveStockItemConfiguration = $this->_objectManager->get(SaveStockItemConfigurationInterface::class);
         $this->handler = $this->_objectManager->get(UpdateReservationsBySkus::class);
         $this->messageEncoder = $this->_objectManager->get(MessageEncoder::class);
         $this->queue = $this->_objectManager->create(Queue::class, ['queueName' => 'inventory.reservations.update']);
         $this->resourceConnection = $this->_objectManager->get(ResourceConnection::class);
         $this->getSourceItemsDataBySku = $this->_objectManager->get(GetSourceItemsDataBySku::class);
+        $this->executeInStoreContext = $this->_objectManager->get(ExecuteInStoreContext::class);
     }
 
     /**
@@ -218,12 +196,12 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
      */
     public function testReservationUpdatedAfterSkuChanged(): void
     {
-        $this->storeManager->setCurrentStore('store_for_eu_website');
-
         $oldSku = 'SKU-1';
         $newSku = 'new-sku';
 
-        $this->orderIdToDelete = $this->placeOrder($oldSku, 4);
+        $this->executeInStoreContext
+            ->execute('store_for_eu_website', [$this, 'placeOrder'], $oldSku, 4);
+
         $this->updateProductSku($oldSku, $newSku);
         $this->productSkuToDelete = $newSku;
 
@@ -259,18 +237,15 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
     private function getCartItem(ProductInterface $product, float $quoteItemQty, int $cartId): CartItemInterface
     {
         /** @var CartItemInterface $cartItem */
-        $cartItem =
-            $this->cartItemFactory->create(
-                [
-                    'data' => [
-                        CartItemInterface::KEY_SKU => $product->getSku(),
-                        CartItemInterface::KEY_QTY => $quoteItemQty,
-                        CartItemInterface::KEY_QUOTE_ID => $cartId,
-                        'product_id' => $product->getId(),
-                        'product' => $product
-                    ]
-                ]
-            );
+        $cartItem = $this->cartItemFactory->create([
+            'data' => [
+                CartItemInterface::KEY_SKU => $product->getSku(),
+                CartItemInterface::KEY_QTY => $quoteItemQty,
+                CartItemInterface::KEY_QUOTE_ID => $cartId,
+                'product_id' => $product->getId(),
+                'product' => $product,
+            ],
+        ]);
 
         return $cartItem;
     }
@@ -300,7 +275,7 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
         $select = $connect->select()->from('inventory_reservation')->where('sku = ?', $sku);
         $result = $connect->fetchRow($select);
 
-        return $result ? $result : [];
+        return $result ?: [];
     }
 
     /**
@@ -327,7 +302,7 @@ class PlaceOrderOnAdditionalStockTest extends AbstractBackendController
      * @param float $itemQty
      * @return int
      */
-    private function placeOrder(string $sku, float $itemQty): int
+    public function placeOrder(string $sku, float $itemQty): int
     {
         $cart = $this->getCart();
         $product = $this->productRepository->get($sku);
