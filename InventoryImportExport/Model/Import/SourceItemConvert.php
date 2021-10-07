45cf100323d3e1ab5eb4e000bd3512e46429ce4d
Oleksandr Iegorov <oiegorov@magento.com>
2021-10-07 13:53:27 -0500

    ACP2E-244: Stock Sources Import: Product goes out of stock if imported with 0 qty

--- InventoryImportExport/Model/Import/SourceItemConvert.php
+++ InventoryImportExport/Model/Import/SourceItemConvert.php
@@ -40,9 +40,10 @@ class SourceItemConvert
             $sourceItem->setSku($rowData[Sources::COL_SKU]);
             $sourceItem->setQuantity((float)$rowData[Sources::COL_QTY]);
 
-            $status = (int)($rowData[Sources::COL_QTY] > 0);
             if (isset($rowData[Sources::COL_STATUS])) {
                 $status = (int)$rowData[Sources::COL_STATUS];
+            } else {
+                $status = 1;
             }
             $sourceItem->setStatus($status);
 
