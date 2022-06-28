66ff48554a07e88e71efc01bd867dc59721088a5
pradeep1819 <pradeep05.pro@gmail.com>
2022-06-28 13:27:32 +0530

    ACP2E-748: Layered navigation filters don't work when "show out of stock" is enabled

---
+++ dev/tests/static/testsuite/Magento/Test/Integrity/_files/extension_dependencies_test/allowed_dependencies/inventory.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+return [
+    'Magento\Elasticsearch' => [
+        'Magento\InventoryCatalogSearch',
+        'Magento\InventoryElasticsearch'
+    ]
+];
