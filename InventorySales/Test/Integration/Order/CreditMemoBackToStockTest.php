95acb8ae2cb32dc3b62ef58c45ad50c6ca26db9f
Anna Bukatar <abukatar@magento.com>
2022-07-01 15:09:46 -0700

    ACP2E-908: Wrong Stock Status after credit memo issued

--- InventorySales/Test/Integration/Order/CreditMemoBackToStockTest.php
+++ InventorySales/Test/Integration/Order/CreditMemoBackToStockTest.php
@@ -10,11 +10,22 @@ namespace Magento\InventorySales\Test\Integration\Order;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Sales\Api\CreditmemoRepositoryInterface;
 use Magento\Sales\Model\Order\CreditmemoFactory;
+use Magento\TestFramework\Fixture\DataFixture;
 use Magento\TestFramework\Fixture\DataFixtureStorage;
 use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\InventoryApi\Api\GetSourceItemsBySkuInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\InventoryApi\Test\Fixture\SourceItem as SourceItemFixture;
+use Magento\Quote\Test\Fixture\GuestCart as GuestCartFixture;
+use Magento\Quote\Test\Fixture\AddProductToCart as AddProductToCartFixture;
+use Magento\Checkout\Test\Fixture\SetBillingAddress as SetBillingAddressFixture;
+use Magento\Checkout\Test\Fixture\SetGuestEmail as SetGuestEmailFixture;
+use Magento\Checkout\Test\Fixture\SetPaymentMethod as SetPaymentMethodFixture;
+use Magento\Checkout\Test\Fixture\PlaceOrder as PlaceOrderFixture;
+use Magento\Sales\Test\Fixture\Invoice as InvoiceFixture;
+
 
 /**
  * Class to test credit memo updates of the reservations
@@ -64,21 +75,20 @@ class CreditMemoBackToStockTest extends TestCase
         $this->getSourceItemBySku = $this->objectManager->get(GetSourceItemsBySkuInterface::class);
     }
 
-    /**
-     * phpcs:disable Generic.Files.LineLength.TooLong
-     *
-     * @magentoDataFixture Magento\Catalog\Test\Fixture\Product with:{"weight": 0, "type_id": "virtual"} as:p1
-     * @magentoDataFixture Magento\CatalogInventory\Test\Fixture\SourceItem with:{"sku": "$p1.sku$", "source_code": "default", "quantity": 100}
-     * @magentoDataFixture Magento\Quote\Test\Fixture\GuestCart as:cart
-     * @magentoDataFixture Magento\Quote\Test\Fixture\AddProductToCart with:{"cart_id": "$cart.id$", "product_id": "$p1.id$", "qty": 1} as:item1
-     * @magentoDataFixture Magento\Checkout\Test\Fixture\SetBillingAddress with:{"cart_id":"$cart.id$"}
-     * @magentoDataFixture Magento\Checkout\Test\Fixture\SetGuestEmail with:{"cart_id":"$cart.id$"}
-     * @magentoDataFixture Magento\Checkout\Test\Fixture\SetPaymentMethod with:{"cart_id":"$cart.id$"}
-     * @magentoDataFixture Magento\Checkout\Test\Fixture\PlaceOrder with:{"cart_id":"$cart.id$"} as:order
-     * @magentoDataFixture Magento\Sales\Test\Fixture\Invoice with:{"order_id":"$order.id$"}
-     *
-     * @return void
-     */
+    #[
+        DataFixture(ProductFixture::class, ['weight' => 0, 'type_id' => 'virtual'], 'p1'),
+        DataFixture(SourceItemFixture::class, ['sku' => '$p1.sku$', 'source_code' => 'default', 'quantity' => 100]),
+        DataFixture(GuestCartFixture::class, as: 'cart'),
+        DataFixture(
+            AddProductToCartFixture::class,
+            ['cart_id' => '$cart.id$', 'product_id' => '$p1.id$', 'qty' => 1], 'item1'
+        ),
+        DataFixture(SetBillingAddressFixture::class, ['cart_id' => '$cart.id$']),
+        DataFixture(SetGuestEmailFixture::class, ['cart_id' => '$cart.id$']),
+        DataFixture(SetPaymentMethodFixture::class, ['cart_id' => '$cart.id$']),
+        DataFixture(PlaceOrderFixture::class, ['cart_id' => '$cart.id$'], 'order'),
+        DataFixture(InvoiceFixture::class, ['order_id' => '$order.id$'])
+    ]
     public function testCreditMemoVirtualProductDoNotReturnToStock(): void
     {
         $productSKU = $this->fixtures->get('p1')->getSku();
