b4d1843f727d83d7527385c63ff5004ff3db4994
magento packaging service <magento-comops@adobe.com>
2022-08-01 16:21:19 -0500

    Version 1.2.3-p2

--- InventoryCatalogAdminUi/Controller/Adminhtml/Source/BulkAssign.php
+++ InventoryCatalogAdminUi/Controller/Adminhtml/Source/BulkAssign.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Magento\InventoryCatalogAdminUi\Controller\Adminhtml\Source;
 
 use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\InventoryCatalogAdminUi\Controller\Adminhtml\Bulk\BulkPageProcessor;
@@ -15,12 +16,12 @@ use Magento\InventoryCatalogAdminUi\Controller\Adminhtml\Bulk\BulkPageProcessor;
 /**
  * Mass assign sources to products.
  */
-class BulkAssign extends Action
+class BulkAssign extends Action implements HttpPostActionInterface
 {
     /**
      * @see _isAllowed()
      */
-    const ADMIN_RESOURCE = 'Magento_Catalog::products';
+    public const ADMIN_RESOURCE = 'Magento_Catalog::products';
 
     /**
      * @var BulkPageProcessor
@@ -41,6 +42,8 @@ class BulkAssign extends Action
     }
 
     /**
+     * Bulk source assignment
+     *
      * @return ResponseInterface|ResultInterface
      */
     public function execute()
