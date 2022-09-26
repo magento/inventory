9a548cabadbe561d1542bd1a05c19cf8379bb4e7
soumah <soumah@adobe.com>
2022-09-26 11:27:45 -0500

    ACP2E-1209: [On-prem] Issue with saving product via admin and in REST API

---
+++ InventoryBundleProduct/Plugin/InventoryApi/UpdateParentStockStatusOnSourceItemsSave.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryBundleProduct\Plugin\InventoryApi;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\InventoryCatalogApi\Model\GetProductIdsBySkusInterface;
+use Magento\InventoryCatalogApi\Model\IsSingleSourceModeInterface;
+use Magento\Bundle\Model\Inventory\ChangeParentStockStatus;
+
+/**
+ * Update stock status of bundle products on children products source items update
+ */
+class UpdateParentStockStatusOnSourceItemsSave
+{
+    /**
+     * @var ChangeParentStockStatus
+     */
+    private $changeParentStockStatus;
+
+    /**
+     * @var GetProductIdsBySkusInterface
+     */
+    private $getProductIdsBySkus;
+
+    /**
+     * @var IsSingleSourceModeInterface
+     */
+    private $isSingleSourceMode;
+
+    /**
+     * @param GetProductIdsBySkusInterface $getProductIdsBySkus
+     * @param ChangeParentStockStatus $changeParentStockStatus
+     * @param IsSingleSourceModeInterface $isSingleSourceMode
+     */
+    public function __construct(
+        GetProductIdsBySkusInterface $getProductIdsBySkus,
+        ChangeParentStockStatus $changeParentStockStatus,
+        IsSingleSourceModeInterface $isSingleSourceMode
+    ) {
+        $this->getProductIdsBySkus = $getProductIdsBySkus;
+        $this->changeParentStockStatus = $changeParentStockStatus;
+        $this->isSingleSourceMode = $isSingleSourceMode;
+    }
+
+    /**
+     * Update stock status of bundle products on children products source items update
+     *
+     * @param SourceItemsSaveInterface $subject
+     * @param void $result
+     * @param SourceItemInterface[] $sourceItems
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterExecute(SourceItemsSaveInterface $subject, $result, array $sourceItems): void
+    {
+        $productIds = [];
+        if ($this->isSingleSourceMode->execute()) {
+            foreach ($sourceItems as $sourceItem) {
+                $sku = $sourceItem->getSku();
+                try {
+                    $productIds[] = (int)$this->getProductIdsBySkus->execute([$sku])[$sku];
+                } catch (NoSuchEntityException $e) {
+                    continue;
+                }
+            }
+            if ($productIds) {
+                $this->changeParentStockStatus->execute($productIds);
+            }
+        }
+    }
+}
