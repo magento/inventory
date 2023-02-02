ace2dfa4e7db9dc0d40fdff614515389fef7536a
AnujNehra <anujnehra@BLR1-LMC-N71227.local>
2023-02-03 00:22:22 +0530

    ACP2E-1571 unit test

---
+++ InventoryInStorePickupSales/Test/Unit/Model/NotifyOrdersAreReadyForPickupTest.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryInStorePickupSales\Test\Unit\Model;
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\InventoryInStorePickupSales\Model\NotifyOrdersAreReadyForPickup;
+use Magento\InventoryInStorePickupSales\Model\Order\AddStorePickupAttributesToOrder;
+use Magento\InventoryInStorePickupSales\Model\Order\CreateShippingDocument;
+use Magento\InventoryInStorePickupSales\Model\Order\Email\ReadyForPickupNotifier;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Api\ShipmentRepositoryInterface;
+use Magento\InventoryInStorePickupSalesApi\Api\Data\ResultInterfaceFactory;
+use Psr\Log\LoggerInterface;
+use PHPUnit\Framework\TestCase;
+use Magento\Sales\Model\Order;
+use Magento\InventoryInStorePickupSalesApi\Api\Data\ResultInterface;
+
+/**
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class NotifyOrdersAreReadyForPickupTest extends TestCase
+{
+    /**
+     * @var NotifyOrdersAreReadyForPickup
+     */
+    private $model;
+
+    /**
+     * @var ReadyForPickupNotifier
+     */
+    private $emailNotifier;
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var AddStorePickupAttributesToOrder
+     */
+    private $addStorePickupAttributesToOrder;
+
+    /**
+     * @var ResultInterfaceFactory
+     */
+    private $resultFactory;
+
+    /**
+     * @var ShipmentRepositoryInterface
+     */
+    private $shipmentRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var CreateShippingDocument
+     */
+    private $createShippingDocument;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var SearchCriteriaInterface
+     */
+    private $searchCriteriaInterfaceMock;
+
+    /**
+     * @var Order
+     */
+    private $orderMock;
+
+    protected function setUp(): void
+    {
+        $this->emailNotifier = $this->getMockBuilder(ReadyForPickupNotifier::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->orderRepository = $this->getMockBuilder(OrderRepositoryInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->addStorePickupAttributesToOrder = $this->getMockBuilder(AddStorePickupAttributesToOrder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->resultFactory = $this->getMockBuilder(ResultInterfaceFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->shipmentRepository = $this->getMockBuilder(ShipmentRepositoryInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getList','get','delete','save','create'])
+            ->addMethods([
+                'getTotalCount'
+            ])
+            ->getMock();
+        $this->searchCriteriaBuilder = $this->getMockBuilder(SearchCriteriaBuilder::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['create','addFilter'])
+            ->getMock();
+        $this->createShippingDocument = $this->getMockBuilder(CreateShippingDocument::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->logger = $this->getMockBuilder(LoggerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->model = new NotifyOrdersAreReadyForPickup(
+            $this->emailNotifier,
+            $this->orderRepository,
+            $this->addStorePickupAttributesToOrder,
+            $this->resultFactory,
+            $this->shipmentRepository,
+            $this->searchCriteriaBuilder,
+            $this->createShippingDocument,
+            $this->logger
+        );
+        $this->searchCriteriaInterfaceMock = $this->getMockBuilder(SearchCriteriaInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->orderMock = $this->getMockBuilder(Order::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->resultMock = $this->getMockBuilder(ResultInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @return void
+     */
+    public function testEmailWithoutException(): void
+    {
+        $this->orderRepository->method('get')->willReturn($this->orderMock);
+        $this->searchCriteriaBuilder->method('addFilter')->willReturnSelf();
+        $this->searchCriteriaBuilder->method('create')->willReturn($this->searchCriteriaInterfaceMock);
+        $this->shipmentRepository->method('getList')->willReturnSelf();
+        $this->resultFactory->method('create')->willReturn($this->resultMock);
+        $this->emailNotifier->expects($this->once())->method('notify');
+        $this->model->execute([1]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testEmailWithException(): void
+    {
+        $this->orderRepository->method('get')->willReturn($this->orderMock);
+        $this->searchCriteriaBuilder->method('addFilter')->willReturnSelf();
+        $this->searchCriteriaBuilder->method('create')->willReturn($this->searchCriteriaInterfaceMock);
+        $this->shipmentRepository->method('getList')->willReturnSelf();
+        $this->resultFactory->method('create')->willReturn($this->resultMock);
+        $this->createShippingDocument->method('execute')->willThrowException(
+            new \Exception("Error")
+        );
+        $this->emailNotifier->expects($this->never())->method($this->anything());
+        $this->model->execute([1]);
+    }
+}
