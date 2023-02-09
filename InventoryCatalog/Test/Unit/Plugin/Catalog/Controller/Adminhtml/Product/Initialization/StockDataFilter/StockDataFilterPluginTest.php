d554f6d69f3955f6c858d4b0ff31d9c8194b25ff
Sarmistha Martha <glo23503@adobe.com>
2023-02-09 19:55:59 +0530

    ACP2E-1517: Admin B2B quote items issue for product with a non-default Inventory Source

---
+++ InventoryCatalog/Test/Unit/Plugin/Catalog/Controller/Adminhtml/Product/Initialization/StockDataFilter/StockDataFilterPluginTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryCatalog\Test\Unit\Plugin\Catalog\Controller\Adminhtml\Product\Initialization\StockDataFilter;
+
+use Magento\InventoryCatalog\Plugin\Catalog\Controller\Adminhtml\Product\Initialization\StockDataFilter\StockDataFilterPlugin;
+use Magento\Catalog\Controller\Adminhtml\Product\Initialization\StockDataFilter;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class StockDataFilterPluginTest extends TestCase
+{
+    /**
+     * @var StockDataFilter|MockObject
+     */
+    private $subjectMock;
+
+    protected function setUp(): void
+    {
+        $this->subjectMock = $this->getMockBuilder(StockDataFilter::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->stockDataFilterPlugin = new StockDataFilterPlugin();
+    }
+
+    /**
+     * @covers StockDataFilterPlugin::afterFilter
+     */
+    public function testAfterFilter(array $inputStockData, array $outputStockData)
+    {
+        $result = [
+            'min_qty' => 0,
+            'max_sale_qty' => 10000,
+            'notify_stock_qty' => 1,
+            'min_sale_qty' => 1,
+            'manage_stock' => 1,
+        ];
+        $stockData = [
+            'min_qty' => 1
+        ];
+        $result = $this->stockDataFilterPlugin->afterFilter($this->subjectMock, $result, $stockData);
+        $this->assertEquals(1, $result['min_qty']);
+        $this->assertEquals(0, $result['qty']);
+    }
+}
