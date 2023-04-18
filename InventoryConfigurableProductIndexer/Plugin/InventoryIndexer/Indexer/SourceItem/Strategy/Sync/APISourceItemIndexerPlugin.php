4bdc72313cf3bd536c28618d70fffbba849f4e09
aplapana <aplapana@adobe.com>
2023-04-18 16:28:33 +0300

    ACP2E-1842: Rest API stock change - implemented solution

---
+++ InventoryConfigurableProductIndexer/Plugin/InventoryIndexer/Indexer/SourceItem/Strategy/Sync/APISourceItemIndexerPlugin.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryConfigurableProductIndexer\Plugin\InventoryIndexer\Indexer\SourceItem\Strategy\Sync;
+
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Model\AbstractModel;
+use Magento\InventoryApi\Api\GetSourceItemsBySkuInterface;
+use Magento\InventoryCatalogApi\Api\DefaultSourceProviderInterface;
+use Magento\InventoryCatalogApi\Model\GetSkusByProductIdsInterface;
+use Magento\InventoryConfigurableProductIndexer\Indexer\SourceItem\SourceItemIndexer;
+
+class APISourceItemIndexerPlugin
+{
+    /**
+     * @var SourceItemIndexer
+     */
+    private SourceItemIndexer $configurableProductsSourceItemIndexer;
+
+    /**
+     * @var GetSourceItemsBySkuInterface
+     */
+    private GetSourceItemsBySkuInterface $getSourceItemsBySku;
+
+    /**
+     * @var DefaultSourceProviderInterface
+     */
+    private DefaultSourceProviderInterface $defaultSourceProvider;
+
+    /**
+     * @var GetSkusByProductIdsInterface
+     */
+    private GetSkusByProductIdsInterface $skuProvider;
+
+    /**
+     * @param SourceItemIndexer $configurableProductsSourceItemIndexer
+     * @param GetSourceItemsBySkuInterface $getSourceItemsBySku
+     * @param DefaultSourceProviderInterface $defaultSourceProvider
+     * @param GetSkusByProductIdsInterface $getSkusByProductIdsInterface
+     */
+    public function __construct(
+        SourceItemIndexer              $configurableProductsSourceItemIndexer,
+        GetSourceItemsBySkuInterface   $getSourceItemsBySku,
+        DefaultSourceProviderInterface $defaultSourceProvider,
+        GetSkusByProductIdsInterface $getSkusByProductIdsInterface
+    ) {
+        $this->configurableProductsSourceItemIndexer = $configurableProductsSourceItemIndexer;
+        $this->getSourceItemsBySku = $getSourceItemsBySku;
+        $this->defaultSourceProvider = $defaultSourceProvider;
+        $this->skuProvider = $getSkusByProductIdsInterface;
+    }
+
+    /**
+     * @param ProductResource $subject
+     * @param $result
+     * @param AbstractModel $object
+     * @return mixed
+     * @throws NoSuchEntityException
+     */
+    public function afterSave(ProductResource $subject, $result, AbstractModel $object)
+    {
+        if ($object instanceof Product && $object->getTypeId() == Configurable::TYPE_CODE) {
+            $childProductIds = $object->getTypeInstance()->getChildrenIds($object->getId());
+            foreach ($childProductIds as $productId) {
+                $childProductSku = $this->skuProvider->execute($productId)[key($productId)];
+                $sourceItems = $this->getSourceItemsBySku->execute($childProductSku);
+                $sourceItemIds = [];
+                foreach ($sourceItems as $key => $sourceItem) {
+                    if ($sourceItem->getSourceCode() === $this->defaultSourceProvider->getCode()) {
+                        unset($sourceItems[$key]);
+                        continue;
+                    }
+                    $sourceItem->setSku($object->getSku());
+                    $sourceItemIds[] = $sourceItem->getId();
+                }
+                if ($sourceItemIds) {
+                    $this->configurableProductsSourceItemIndexer->executeList($sourceItemIds);
+                }
+            }
+        }
+
+        return $result;
+    }
+}
