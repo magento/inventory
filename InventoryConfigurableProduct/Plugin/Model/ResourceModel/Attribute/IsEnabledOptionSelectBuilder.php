6c9f5bc709891eddee15ddd4b8cf8310f28cee1d
soumah <soumah@adobe.com>
2023-10-05 17:49:26 -0500

    ACP2E-2255: Garphql call resturn disabled simple produuct option while fetching config product

--- InventoryConfigurableProduct/Plugin/Model/ResourceModel/Attribute/IsEnabledOptionSelectBuilder.php
+++ InventoryConfigurableProduct/Plugin/Model/ResourceModel/Attribute/IsEnabledOptionSelectBuilder.php
@@ -7,9 +7,11 @@ declare(strict_types=1);
 
 namespace Magento\InventoryConfigurableProduct\Plugin\Model\ResourceModel\Attribute;
 
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
 use Magento\ConfigurableProduct\Model\ResourceModel\Attribute\OptionSelectBuilderInterface;
 use Magento\Framework\DB\Select;
+use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\Store;
@@ -22,25 +24,15 @@ use Magento\Catalog\Model\Product\Attribute\Source\Status as ProductStatus;
 class IsEnabledOptionSelectBuilder
 {
     /**
-     * @var StoreManagerInterface
-     */
-    private $storeManager;
-
-    /**
-     * @var ProductAttributeRepositoryInterface
-     */
-    private $attributeRepository;
-
-    /**
      * @param StoreManagerInterface $storeManager
      * @param ProductAttributeRepositoryInterface $attributeRepository
+     * @param MetadataPool $metadataPool
      */
     public function __construct(
-        StoreManagerInterface $storeManager,
-        ProductAttributeRepositoryInterface $attributeRepository,
+        private readonly StoreManagerInterface $storeManager,
+        private readonly ProductAttributeRepositoryInterface $attributeRepository,
+        private readonly MetadataPool $metadataPool
     ) {
-        $this->storeManager = $storeManager;
-        $this->attributeRepository = $attributeRepository;
     }
 
     /**
@@ -56,29 +48,28 @@ class IsEnabledOptionSelectBuilder
      */
     public function afterGetSelect(
         OptionSelectBuilderInterface $subject,
-        Select $select
+        Select $select,
     ) {
         $storeId = $this->storeManager->getStore()->getId();
-        $statusAttributeId = $this->attributeRepository->get('status')->getAttributeId();
+        $status = $this->attributeRepository->get(ProductInterface::STATUS);
+        $metadata = $this->metadataPool->getMetadata(ProductInterface::class);
+        $linkField = $metadata->getLinkField();
 
-        $select->where(
-            '((IF(EXISTS
-                    (SELECT value
-                        FROM `catalog_product_entity_int` AS `product_entity_int`
-                        WHERE product_entity_int.store_id = ' . $storeId . '
-                            AND product_entity_int.attribute_id = ' . $statusAttributeId . '
-                            AND product_entity_int.row_id = entity.row_id),
-                    (SELECT `value`
-                        FROM `catalog_product_entity_int` AS `product_entity_int`
-                        WHERE product_entity_int.store_id = ' . $storeId . '
-                            AND product_entity_int.attribute_id = ' . $statusAttributeId . '
-                            AND product_entity_int.row_id = entity.row_id),
-                    (SELECT `value`
-                        FROM `catalog_product_entity_int` AS `product_entity_int`
-                        WHERE product_entity_int.store_id = ' . Store::DEFAULT_STORE_ID . '
-                            AND product_entity_int.attribute_id = ' . $statusAttributeId . '
-                            AND product_entity_int.row_id = entity.row_id)))
-                    = ' . ProductStatus::STATUS_ENABLED . ')'
+        $select->joinInner(
+            ['entity_status_global' => $status->getBackendTable()],
+            "entity_status_global.{$linkField} = entity.{$linkField}"
+            . " AND entity_status_global.attribute_id = {$status->getAttributeId()}"
+            . " AND entity_status_global.store_id = " . Store::DEFAULT_STORE_ID,
+            []
+        )->joinLeft(
+            ['entity_status_store' => $status->getBackendTable()],
+            "entity_status_store.{$linkField} = entity.{$linkField}"
+            . " AND entity_status_store.attribute_id = {$status->getAttributeId()}"
+            . " AND entity_status_store.store_id = {$storeId}",
+            []
+        )->where(
+            $select->getConnection()->getIfNullSql('entity_status_global.value', 'entity_status_store.value') . ' = ?',
+            ProductStatus::STATUS_ENABLED
         );
 
         return $select;
