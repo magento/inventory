cbffc4de9de546e65dbe806789cac85252a86800
Oleksandr Iegorov <oiegorov@adobe.com>
2024-02-21 13:36:58 -0600

    ACP2E-767: Remove Magento Catalog dependency on Magento Inventory

---
+++ InventoryCatalog/Model/SortingAdjustment.php
@@ -0,0 +1,58 @@
+<?php
+/************************************************************************
+ *
+ * Copyright 2024 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of Adobe and its suppliers, if any. The intellectual
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
+ * ************************************************************************
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryCatalog\Model;
+
+use Magento\Framework\Indexer\Config\Converter\SortingAdjustmentInterface;
+use Magento\Catalog\Model\Indexer\Product\Price\Processor as PriceIndexer;
+use Magento\InventoryIndexer\Indexer\InventoryIndexer;
+
+class SortingAdjustment implements SortingAdjustmentInterface
+{
+    /**
+     * @inheirtdoc
+     */
+    public function adjust(array $indexersList): array
+    {
+        $indexersListAdjusted = [];
+        $order = array_keys($indexersList);
+
+        $pricePos = array_search(PriceIndexer::INDEXER_ID, $order);
+        $inventoryPos = array_search(InventoryIndexer::INDEXER_ID, $order);
+        if ($pricePos !== false && $inventoryPos !== false && $inventoryPos > $pricePos) {
+            $newOrder = [];
+            foreach ($order as $pos => $indexerId) {
+                if ($pos < $pricePos || $pos > $inventoryPos) {
+                    $newOrder[$pos] = $indexerId;
+                } elseif ($pos === $pricePos) {
+                    $newOrder[$pos] = $order[$inventoryPos];
+                    $newOrder[$pos+1] = $indexerId;
+                } elseif ($pos > $pricePos && $pos < $inventoryPos) {
+                    $newOrder[$pos+1] = $indexerId;
+                }
+            }
+            for ($i = 0; $i < count($newOrder); $i++) {
+                $indexersListAdjusted[$newOrder[$i]] = $indexersList[$newOrder[$i]];
+            }
+        } else {
+            $indexersListAdjusted = $indexersList;
+        }
+
+        return $indexersListAdjusted;
+    }
+}
